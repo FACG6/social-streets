ab6c7209e5adba84322b52974e53c0fca90b78a2
Ahmed I. Abdellatif <ahmedisam9922@gmail.com>
2019-05-28 12:21:27 +0300

    create the header component

---
+++ client/src/components/Header/index.jsx
@@ -0,0 +1,74 @@
+import React, { Component } from "react";
+import { BrowserRouter as Router, Link } from "react-router-dom";
+import PropTypes from "prop-types";
+import * as Logo from "./logo.png";
+import HamburgerButton from "./../HamburgerButton";
+import "./style.css";
+
+function Menu({ show }) {
+  if (show)
+    return (
+      <Router>
+        <div className="header__menu">
+          <Link to="/profile" className="header__menu-text">
+            Profile
+          </Link>
+          <Link to="/create-post" className="header__menu-text">
+            Create Post
+          </Link>
+          <Link to="/posts/live" className="header__menu-text">
+            Live Posts
+          </Link>
+          <Link to="/posts/drafts" className="header__menu-text">
+            Drafts
+          </Link>
+          <a href="/logout" className="header__menu-text">
+            Log out
+          </a>
+        </div>
+      </Router>
+    );
+  else return <></>;
+}
+
+export default class Header extends Component {
+  state = {
+    showMenu: false
+  };
+
+  toggleMenu = () => {
+    const { showMenu } = this.state;
+    this.setState({ showMenu: !showMenu });
+  };
+
+  render() {
+    const { showMenu } = this.state;
+    const { showHamburger } = this.props;
+    return (
+      <header className="header">
+        <img src={Logo} alt="Logo" className="header__logo" />
+        {showHamburger ? (
+          <HamburgerButton
+            className="header__hamburger-button"
+            toggleMenu={this.toggleMenu}
+          />
+        ) : (
+          <></>
+        )}
+        <Menu show={showMenu} />
+      </header>
+    );
+  }
+}
+
+Menu.propTypes = {
+  show: PropTypes.bool.isRequired
+};
+
+Header.propTypes = {
+  showHamburger: PropTypes.bool
+};
+
+Header.defaultProps = {
+  showHamburger: true
+};
