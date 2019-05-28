3ee1f905caeec96d5042288c1651fdbdc1f3e3c9
aminalakhsham <aminking140@gmail.com>
2019-05-28 20:03:29 +0300

    add functional component return footer

---
+++ client/src/components/footer/index.js
@@ -0,0 +1,48 @@
+import React from "react";
+import "./style.css";
+import { Icon } from "antd";
+
+export const Footer = () => {
+  const socialLinks = {
+    FB: "https://www.facebook.com/socialstreetsco",
+    instagram: "https://www.instagram.com/socialstreetsco/",
+    twitter: "https://twitter.com/socialstreetsco",
+    linkedIn: "https://www.linkedin.com/company/10668755/"
+  };
+  return (
+    <footer className="footer">
+      <a
+        className="footer--a"
+        target="_blank"
+        rel="noopener noreferrer"
+        href={socialLinks.FB}
+      >
+        <Icon type="facebook" />
+      </a>
+      <a
+        className="footer--a"
+        target="_blank"
+        rel="noopener noreferrer"
+        href={socialLinks.linkedIn}
+      >
+        <Icon type="linkedin" />
+      </a>
+      <a
+        className="footer--a"
+        target="_blank"
+        rel="noopener noreferrer"
+        href={socialLinks.instagram}
+      >
+        <Icon type="instagram" />
+      </a>
+      <a
+        className="footer--a"
+        target="_blank"
+        rel="noopener noreferrer"
+        href={socialLinks.twitter}
+      >
+        <Icon type="twitter" />
+      </a>
+    </footer>
+  );
+};
