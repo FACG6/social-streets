58fdb0961a06e41f55f234545978dfb448d2d130
AbdallahAmmar96 <abodsaid1996@gmail.com>
2019-05-29 14:46:30 +0300

    add data for the select

---
+++ client/src/components/pages/profileBusiness/static.js
@@ -0,0 +1,60 @@
+const residences = [
+  {
+    value: 'zhejiang',
+    label: 'Zhejiang',
+    children: [
+      {
+        value: 'hangzhou',
+        label: 'Hangzhou',
+        children: [
+          {
+            value: 'xihu',
+            label: 'West Lake',
+          },
+        ],
+      },
+    ],
+  },
+  {
+    value: 'jiangsu',
+    label: 'Jiangsu',
+    children: [
+      {
+        value: 'nanjing',
+        label: 'Nanjing',
+        children: [
+          {
+            value: 'zhonghuamen',
+            label: 'Zhong Hua Men',
+          },
+        ],
+      },
+    ],
+  },
+];
+
+const BusinesTypeValues = [
+  {
+    key: 1,
+    value: "idfghkn"
+  },
+  {
+    key: 2,
+    value: "kfdgnknf"
+  },
+  {
+    key: 3,
+    value: "kdfngkn"
+  },
+  {
+    key: 4,
+    value: "fkgmjfg pkerpke"
+  },
+  {
+    key: 5,
+    value: "dkfngjkfm"
+  }
+];
+
+export { residences, BusinesTypeValues }
+
