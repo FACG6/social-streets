a4276ef29aa58de95dac88eaa8618d7740c48448
Israa <israa96sulaiman@gmail.com>
2019-05-29 12:01:48 +0300

    remove unnecessary style from app.css

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,15 @@
 import React from "react";
 import "./App.css";
+import Posts from './components/Posts'
 
 function App() {
   return (
-    <>
+    <div className='app'>
+    <div style={{background:'black', height: '115px'}}>Header</div>
+    <Posts />
+    <div style={{ background: 'black', height: '70px' }}>Header</div>
 
-    </>
+    </div>
   );
 }
 
