4078d2da63a6e3e4a0b92b803e65a0dd1db0ab63
Israa <israa96sulaiman@gmail.com>
2019-05-29 12:05:18 +0300

    remove unnecessary code from app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,11 @@
 import React from "react";
 import "./App.css";
-import Posts from './components/Posts'
 
 function App() {
   return (
-    <div className='app'>
-    <div style={{background:'black', height: '115px'}}>Header</div>
-    <Posts />
-    <div style={{ background: 'black', height: '70px' }}>Header</div>
-
-    </div>
+    <>
+      
+    </>
   );
 }
 
