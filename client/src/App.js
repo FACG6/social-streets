ecfab55a69afb0c5b70e11e2a15db72fb32f4c63
AbdallahAmmar96 <abodsaid1996@gmail.com>
2019-05-29 14:44:58 +0300

    edit the render

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,12 @@
 import React from "react";
+
+import BusinessProfile from './components/pages/profileBusiness'
+
 import "./App.css";
 function App() {
   return (
     <>
-
+      <BusinessProfile />
     </>
   );
 }
