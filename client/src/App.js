15b9c2aa4573fcae3082fb4b48ebcd9ffb37c3a1
AbdallahAmmar96 <abodsaid1996@gmail.com>
2019-05-29 10:11:10 +0300

    render the component in the app

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
 import "./App.css";
+import Personal from './component/contanier/Personal_Profile'
 
 function App() {
   return (
     <>
-
+      <Personal />
     </>
   );
 }
