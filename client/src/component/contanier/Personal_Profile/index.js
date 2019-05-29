3b7778901761bc0bdee6908e649ee4f0e635e047
AbdallahAmmar96 <abodsaid1996@gmail.com>
2019-05-29 12:30:45 +0300

    edit the form by use antd

--- client/src/component/contanier/Personal_Profile/index.js
+++ client/src/component/contanier/Personal_Profile/index.js
@@ -1,90 +1,183 @@
 import React from 'react'
-import * as Yup from 'yup'
-import { withFormik, Field } from 'formik'
-import { Button, Input } from 'antd';
+import { Form, Input, Button, } from 'antd';
 
 import 'antd/dist/antd.css'
 import './style.css'
 
-class Personal extends React.Component {
+class RegistrationForm extends React.Component {
+
+  state = {
+    confirmDirty: false,
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    this.props.form.validateFieldsAndScroll((err, values) => {
+      if (!err) {
+        console.log('Received values of form: ', values);
+      }
+    });
+  };
+
+  handleConfirmBlur = e => {
+    const value = e.target.value;
+    this.setState({ confirmDirty: this.state.confirmDirty || !!value });
+  };
+
+  compareToFirstPassword = (rule, value, callback) => {
+    const form = this.props.form;
+    if (value && value !== form.getFieldValue('password')) {
+      callback('Password must match!');
+    } else {
+      callback();
+    }
+  };
+
+  validateToNextPassword = (rule, value, callback) => {
+    const form = this.props.form;
+    if (value && this.state.confirmDirty) {
+      form.validateFields(['confirm'], { force: true });
+    }
+    callback();
+  };
 
   render() {
+    const { getFieldDecorator } = this.props.form;
 
-    const { handleSubmit, handleChange } = this.props
-    const { errors, touched } = this.props
+    const formItemLayout = {
+      labelCol: {
+        xs: { span: 0 },
+        sm: { span: 0 },
+      },
+      wrapperCol: {
+        xs: { span: 0 },
+        sm: { span: 24 },
+      },
+    };
+    const tailFormItemLayout = {
+      wrapperCol: {
+        xs: {
+          span: 24,
+          offset: 0,
+        },
+        sm: {
+          span: 16,
+          offset: 8,
+        },
+      },
+    };
 
     return (
-      <form onSubmit={handleSubmit} className='form'>
+      <Form {...formItemLayout} onSubmit={this.handleSubmit}>
 
-          <Field
-            name='firstName'
-            render={({ field }) => (
-              <>
-                <label className='form-label' htmlFor='first'> First Name </label>
-                <Input {...field} onChange={handleChange} id='first' placeholder="First Name" className='form-input' />
-                {touched.firstName && errors.firstName && <h6 className='form-error' >{errors.firstName}</h6>}
-              </>
-            )}
-          />
+        <Form.Item label="First Name" className='form--item'>
+          {getFieldDecorator('first', {
+            rules: [
+              {
+                type: 'string',
+                message: 'The First Name should be string!',
+              },
+              {
+                whitespace: true,
+                message: 'Delete the spaces!'
+              },
+              {
+                min: 3,
+                message: 'First Name must be 3 charcter at least!',
+              },
+              {
+                required: true,
+                message: 'Please inter your First Name!',
+              },
+            ],
+          })(<Input placeholder='' />)}
+        </Form.Item>
 
-          <Field
-            name='lastName'
-            render={({ field }) => (
-              <>
-                <label className='form-label' htmlFor='last' > Last Name</label>
-                <Input {...field} onChange={handleChange} id='last' placeholder="Last Name" className='form-input' />
-                {touched.lastName && errors.lastName && <h6 className='form-error' >{errors.lastName}</h6>}
-              </>
-            )}
-          />
+        <Form.Item label="Last Name" className='form--item' >
+          {getFieldDecorator('last', {
+            rules: [
+              {
+                type: 'string',
+                message: 'The Last Name should be string!',
+              },
+              {
+                whitespace: true,
+                message: 'Delete the spaces!'
+              },
+              {
+                min: 3,
+                message: 'Last Name must be 3 charcter at least!',
+              },
+              {
+                required: true,
+                message: 'Please inter your Last Name!',
+              },
+            ],
+          })(<Input />)}
+        </Form.Item>
 
-          <Field
-            name='email'
-            render={({ field }) => (
-              <>
-                <label className='form-label' htmlFor='email' > Email </label>
-                <Input {...field} id='email' placeholder="Email" className='form-input' />
-                {touched.email && errors.email && <h6 className='form-error' >{errors.email}</h6>}
-              </>
-            )}
-          />
+        <Form.Item label="E-mail" className='form--item' >
+          {getFieldDecorator('email', {
+            rules: [
+              {
+                type: 'email',
+                message: 'The input is not valid E-mail!',
+              },
+              {
+                required: true,
+                message: 'Please input your E-mail!',
+              },
+            ],
+          })(<Input />)}
+        </Form.Item>
 
-          <Field
-            name='password'
-            render={({ field }) => (
-              <>
-                <label className='form-label' htmlFor='pass' > Password </label>
-                <Input.Password {...field} id='pass' placeholder="Password" className='form-input' />
-                {touched.password && errors.password && <h6 className='form-error' >{errors.password}</h6>}
-              </>
-            )}
-          />
+        <Form.Item label="Password" hasFeedback className='form--item' >
+          {getFieldDecorator('password', {
+            rules: [
+              {
+                required: true,
+                message: 'Please input your password!',
+              },
+              {
+                whitespace: true,
+                message: 'Delete the spaces!'
+              },
+              {
+                validator: this.validateToNextPassword,
+              },
+              {
+                min: 8,
+                message: 'Password must be 8 charcter at least!',
+              }
+            ],
+          })(<Input.Password />)}
+        </Form.Item>
 
-        <Button type="primary" htmlType="submit" className='form-btn--save' block>Save</Button>
-        <Button type="primary" className='form-btn--cancel' block>Cancel</Button>
-      </form>
-    )
+        <Form.Item label="Confirm Password" hasFeedback className='form--item' >
+          {getFieldDecorator('confirm', {
+            rules: [
+              {
+                required: true,
+                message: 'Please confirm your password!',
+              },  
+              {
+                validator: this.compareToFirstPassword,
+              },
+            ],
+          })(<Input.Password onBlur={this.handleConfirmBlur} />)}
+        </Form.Item>
+
+        <Form.Item {...tailFormItemLayout}>
+          <Button type="primary" htmlType="submit">
+            Register
+          </Button>
+        </Form.Item>
+
+      </Form>
+    );
   }
 }
 
-const PersonalFormik = withFormik({
-  mapPropsToValues({ firstName, lastName, email, password }) {
-    return {
-      firstName,
-      lastName,
-      email,
-      password
-    }
-  },
-  validationSchema: Yup.object().shape({
-    email: Yup.string().email('Email is not valid').required('Email is requierd'),
-    firstName: Yup.string('First Name must be a string').required('First Name is requierd'),
-    lastName: Yup.string().required('Last Name is requierd'),
-    password: Yup.string().min(5, 'Password must be 5 charcter at least').required('Password is requierd')
-  }),
-  handleSubmit(values) {
-    console.log(values)
-  },
-})(Personal)
+const WrappedRegistrationForm = Form.create({ name: 'register' })(RegistrationForm);
 
-export default PersonalFormik
+export default WrappedRegistrationForm
