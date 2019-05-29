91d43101ac567a809efd99af5461221134a83f4f
Ahmed I. Abdellatif <ahmedisam9922@gmail.com>
2019-05-29 12:14:38 +0300

    structure the login form using ant.design

--- client/src/components/parts/LoginForm/index.jsx
+++ client/src/components/parts/LoginForm/index.jsx
@@ -1,15 +1,27 @@
 import React, { Component, createRef } from "react";
+import { BrowserRouter as Router, Link } from "react-router-dom";
 import PropTypes from "prop-types";
+import { Form, Icon, Input, Checkbox } from "antd";
 import Button from "./../Button";
 import "./style.css";
+import "antd/dist/antd.css";
 
-export default class LoginForm extends Component {
+class LoginForm extends Component {
   constructor(props) {
     super(props);
     this.handleSubmit = this.handleSubmit.bind(this);
     this.emailInput = createRef();
     this.passwordInput = createRef();
   }
+
+  handleSubmit = e => {
+    e.preventDefault();
+    this.props.form.validateFields((err, values) => {
+      if (!err) {
+        console.log("Received values of form: ", values);
+      }
+    });
+  };
 
   handleSubmit() {
     const user = JSON.stringify({
@@ -20,35 +32,54 @@ export default class LoginForm extends Component {
   }
 
   render() {
+    const { getFieldDecorator } = this.props.form;
     return (
-      <form className={`login__form ${this.props.className}`}>
-        <label htmlFor="emailInput" className="login__label login__email-label">
-          <span className="login__label-text login__email-text">
-            Email Address
-          </span>
-          <input
-            type="email"
-            id="emailInput"
-            className="login__input login__email-input"
-            ref={this.emailInput}
-          />
-        </label>
-        <label
-          htmlFor="passwordInput"
-          className="login__label login__password-label"
-        >
-          <span className="login__label-text login__password-text">
-            Password
-          </span>
-          <input
-            type="password"
-            id="passwordInput"
-            className="login__input login__password-input"
-            ref={this.passwordInput}
-          />
-        </label>
-        <Button onClick={this.handleSubmit}>Log in</Button>
-      </form>
+      <Form onSubmit={this.handleSubmit} className="login-form">
+        <Form.Item>
+          {getFieldDecorator("username", {
+            rules: [{ required: true, message: "Please input your username!" }]
+          })(
+            <Input
+              prefix={<Icon type="user" style={{ color: "rgba(0,0,0,.25)" }} />}
+              placeholder="Username"
+            />
+          )}
+        </Form.Item>
+        <Form.Item>
+          {getFieldDecorator("password", {
+            rules: [{ required: true, message: "Please input your Password!" }]
+          })(
+            <Input
+              prefix={<Icon type="lock" style={{ color: "rgba(0,0,0,.25)" }} />}
+              type="password"
+              placeholder="Password"
+            />
+          )}
+        </Form.Item>
+        <Form.Item>
+          {getFieldDecorator("remember", {
+            valuePropName: "checked",
+            initialValue: true
+          })(<Checkbox>Remember me</Checkbox>)}
+          <a className="login-form-forgot" href="">
+            Forgot password
+          </a>
+          {/* <Button
+            type="primary"
+            htmlType="submit"
+            className="login-form-button"
+          >
+            Log in
+          </Button> */}
+          <Button onClick={this.handleSubmit}>Log in</Button>
+          Or
+          <Router>
+            <Link to="/signup">register now!</Link>
+          </Router>
+        </Form.Item>
+      </Form>
     );
   }
 }
+
+export default Form.create({ name: "Login_Form" })(LoginForm);
