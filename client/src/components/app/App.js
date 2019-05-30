840dfdc4a1678a21985483713bacd8eaf03bb2f0
Ahmed I. Abdellatif <ahmedisam9922@gmail.com>
2019-05-30 11:59:12 +0300

    fix App.js

--- client/src/components/app/App.js
+++ client/src/components/app/App.js
@@ -3,14 +3,8 @@ import "antd/dist/antd.css";
 
 import "./App.css";
 
-import Login from "components/pages/LoginPage/index";
-
 function App() {
-  return (
-    <>
-      <Login />
-    </>
-  );
+  return <></>;
 }
 
 export default App;
