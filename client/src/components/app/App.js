fdd5aeb06747254514701f46fb5fe872192f03a8
aminalakhsham <aminking140@gmail.com>
2019-05-30 15:43:53 +0300

    add header and footer in app

--- client/src/components/app/App.js
+++ client/src/components/app/App.js
@@ -2,9 +2,19 @@ import React from "react";
 import "antd/dist/antd.css";
 
 import "./App.css";
+import { Header, Footer } from "components/utils";
+import { Home } from "components/pages";
 
 function App() {
-  return <></>;
+  return (
+    <div>
+      <Header />
+      <main className="container">
+        <Home />
+      </main>
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
--- client/src/components/utils/index.js
+++ client/src/components/utils/index.js
@@ -1,3 +1,20 @@
+import Header from "./Header";
 import Footer from "./footer";
 import Anchor from "./anchor";
+<<<<<<< Updated upstream
 export { Footer, Anchor };
+=======
+import Button from "./Button";
+import InputAntd from "./InputAntd";
+import TextAreaAntd from "./TextAreaAntd";
+import DropDownAntd from "./DropDownAntd";
+export {
+  Header,
+  Footer,
+  Anchor,
+  Button,
+  InputAntd,
+  TextAreaAntd,
+  DropDownAntd
+};
+>>>>>>> Stashed changes
