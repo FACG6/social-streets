28a1ffbae623a7b46e769e2c8b7f0da9b6138b80
Israa <israa96sulaiman@gmail.com>
2019-05-30 10:55:39 +0300

    edit live post style Relates #7

--- client/src/components/app/App.js
+++ client/src/components/app/App.js
@@ -2,9 +2,12 @@ import React from "react";
 import "antd/dist/antd.css";
 
 import "./App.css";
+import Header from '../utils/Header'
+import Footer from 'components/utils/footer'
+import LivePosts from '../pages/LivePosts'
 
 function App() {
-  return <></>;
+  return <><Header /> <LivePosts /><Footer /></>;
 }
 
 export default App;
