0f917e7a6acae4c397f3f5de935412a8f39a7f1c
Israa <israa96sulaiman@gmail.com>
2019-05-30 11:19:54 +0300

    remove unnecessary code from app

--- client/src/components/app/App.js
+++ client/src/components/app/App.js
@@ -2,12 +2,9 @@ import React from "react";
 import "antd/dist/antd.css";
 
 import "./App.css";
-import DraftPosts from 'components/pages/DraftPosts'
-import Header from 'components/utils/Header'
-import Footer from 'components/utils/footer'
 
 function App() {
-  return <><Header /><DraftPosts /><Footer /></>;
+  return <></>;
 }
 
 export default App;
