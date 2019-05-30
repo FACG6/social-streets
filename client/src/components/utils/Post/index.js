e70395710319a67eac85d895800e7d017ba01160
Israa <israa96sulaiman@gmail.com>
2019-05-30 08:29:39 +0300

    create Post component Relates #24

---
+++ client/src/components/utils/Post/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Icon } from 'antd';
+import './style.css';
+
+export default function Post({ postType, className }) {
+  return (
+    <button className={`${className} posts--button posts--button-white`}>
+      <Icon className='posts--icon posts--form-icon' type='form' />
+      {postType}
+    </button>
+  )
+}
