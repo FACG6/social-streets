3d19f6f464eef1abe2b8c2a1cb8522ad31606539
Israa <israa96sulaiman@gmail.com>
2019-06-01 22:17:57 +0300

    create componentDidMount to get published post and render it Relates #14

--- client/src/components/pages/PublicService/index.js
+++ client/src/components/pages/PublicService/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Icon } from 'antd';
+import publicService from './dummyData'
 
 import './style.css';
 
@@ -8,7 +9,17 @@ export default class PublicService extends Component {
   state = {
     publicService: {},
   }
+  componentDidMount() {
+    if (this.props.postStatus === 'published') {
+      //fetch the public-service post//
+      this.setState({ publicService });
+    }
+  }
   render() {
+    let post = this.props.publicService;
+    if (this.props.postStatus === 'published') {
+      post = this.state.publicService;
+    }
     const {
       image,
       title,
@@ -23,57 +34,61 @@ export default class PublicService extends Component {
       link,
       linkImg,
       secondaryTags,
-    } = this.props.publicService;
+    } = post;
 
-    const pargraphs = description.split('\n');
+    const pargraphs = description ? description.split('\n') : null;
 
     return (
       <section className='public-service-page--main'>
-        <img className='public-service--thumbnail-img' src={image} alt={title}></img>
-        <h1 className='public-service--title'>{title}</h1>
-        <div className='entry-meta-container'>
-          <div className='entry-meta-container--item'>
-            <Icon type='calendar' className='entry-meta-container--icon' />
-            <span className='entry-meta-container--meta'>{publishDate}</span>
-          </div>
-          <div className='entry-meta-container--item'>
-            <Icon type='user' className='entry-meta-container--icon' />
-            <span className='entry-meta-container--meta'>{publisher}</span>
-          </div>
-          <div className='entry-meta-container--item'>
-            <Icon type='tags' className='entry-meta-container--icon' />
-            <span className='entry-meta-container--meta'>{primaryTag}</span>
-          </div>
-          <div className='entry-meta-container--item'>
-            <Icon type='form' className='entry-meta-container--icon' />
-            <span className='entry-meta-container--meta'>{contentType}</span>
-          </div>
-        </div>
-        <div className='public-service--content'>
-          {pargraphs.map((paragraph, index) => {
-            return <p key={index} className='public-service--paragraph'>{paragraph}</p>
-          })}
-        </div>
-        <div className='public-service--organizer'>
-          <span className='public-service--label'>Organizer Website</span>
-          <span className='public-service--website'>{organizerWebsite}</span>
-        </div>
-        <div className='public-service--tags-container'>
-          <span className='public-service--label'>Tags</span>
-          <div className='public-service--tags'>
-            <span className='public-service--tag'>{primaryTag}</span>
-            {secondaryTags.map(tag => <span className='public-service--tag'>{tag}</span>)}
-          </div>
-        </div>
-        <div className='public-service--thumbnail-container'>
-          <span className='public-service--label public-service--label--link'>Link</span>
-          <div className='public-service--thumbnail-link'>
-            <img className='thumbnail-link--image' src={linkImg} alt='link Title'></img>
-            <h1 className='thumbnail-link--title'>{linkTitle}</h1>
-            <a href={link} className='thumbnail-link--link'>{link}</a>
-            <p className='thumbnail-link--description'>{linkDescription}</p>
-          </div>
-        </div>
+        {post.title ?
+          <>
+            <img className='public-service--thumbnail-img' src={image} alt={title}></img>
+            <h1 className='public-service--title'>{title}</h1>
+            <div className='post-header'>
+              <div className='mpost-header--item'>
+                <Icon type='calendar' className='post-header--icon' />
+                <span className='post-header--meta'>{publishDate}</span>
+              </div>
+              <div className='post-header--item'>
+                <Icon type='user' className='post-header--icon' />
+                <span className='post-header--meta'>{publisher}</span>
+              </div>
+              <div className='post-header--item'>
+                <Icon type='tags' className='post-header--icon' />
+                <span className='post-header--meta'>{primaryTag}</span>
+              </div>
+              <div className='post-header--item'>
+                <Icon type='form' className='post-header--icon' />
+                <span className='post-header--meta'>{contentType}</span>
+              </div>
+            </div>
+            <div className='public-service--content'>
+              {pargraphs.map((paragraph, index) => {
+                return <p key={index} className='public-service--paragraph'>{paragraph}</p>
+              })}
+            </div>
+            <div className='public-service--organizer'>
+              <span className='public-service--label'>Organizer Website</span>
+              <span className='public-service--website'>{organizerWebsite}</span>
+            </div>
+            <div className='public-service--tags-container'>
+              <span className='public-service--label'>Tags</span>
+              <div className='public-service--tags'>
+                <span className='public-service--tag'>{primaryTag}</span>
+                {secondaryTags.map((tag, index) => <span key={index} className='public-service--tag'>{tag}</span>)}
+              </div>
+            </div>
+            <div className='public-service--thumbnail-container'>
+              <span className='public-service--label public-service--label--link'>Link</span>
+              <div className='public-service--thumbnail-link'>
+                <img className='thumbnail-link--image' src={linkImg} alt='link Title'></img>
+                <h1 className='thumbnail-link--title'>{linkTitle}</h1>
+                <a href={link} className='thumbnail-link--link'>{link}</a>
+                <p className='thumbnail-link--description'>{linkDescription}</p>
+              </div>
+            </div>
+          </> : <span>...Loading</span>
+        }
       </section>
     )
   }
