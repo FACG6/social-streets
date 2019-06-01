ee3835fe7d18a3ababaade22095e3ac95ffb4b7c
aminalakhsham <aminking140@gmail.com>
2019-06-01 17:22:32 +0300

    remove app from PR

--- client/src/components/app/App.js
+++ client/src/components/app/App.js
@@ -1,15 +1,10 @@
 import React from "react";
 import "antd/dist/antd.css";
 
-import { PostForm } from "components/pages";
 import "./App.css";
 
 function App() {
-  return (
-    <>
-      <PostForm postFormType="Public Services" id="1" />
-    </>
-  );
+  return <></>;
 }
 
 export default App;
