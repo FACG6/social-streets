5016fbb07c17695e1c4623b23c8f9dacaa6e7297
AbdallahAmmar96 <abodsaid1996@gmail.com>
2019-06-01 09:11:48 +0300

    add the input group for the inputs

--- client/src/components/utils/personalForm/index.js
+++ client/src/components/utils/personalForm/index.js
@@ -4,6 +4,8 @@ import { Form, Input } from 'antd'
 import Button from 'components/utils/Button'
 import './style.css'
 
+const InputGroup = Input.Group;
+
 class PersonalForm extends React.Component {
 
   state = {
@@ -53,127 +55,157 @@ class PersonalForm extends React.Component {
   render() {
     const { getFieldDecorator } = this.props.form;
 
-    const formItemLayout = {
-      labelCol: {
-        xs: { span: 0 },
-        sm: { span: 0 },
-      },
-      wrapperCol: {
-        xs: { span: 0 },
-        sm: { span: 24 },
-      },
-    };
-
     return (
-      <Form {...formItemLayout}  onSubmit={this.handleSubmit} className='form'>
+      <Form onSubmit={this.handleSubmit} className='form'>
 
-        <Form.Item label="First Name" className='form--item'>
-          {getFieldDecorator('first', {
-            rules: [
-              {
-                type: 'string',
-                message: 'The First Name should be string!',
-              },
-              {
-                whitespace: true,
-                message: 'Delete the spaces!'
-              },
-              {
-                min: 3,
-                message: 'First Name must be 3 charcter at least!',
-              },
-              {
-                required: true,
-                message: 'Please inter your First Name!',
-              },
-            ],
-          })(<Input placeholder='Your First Name' />)}
-        </Form.Item>
+        <InputGroup size="large">
 
-        <Form.Item label="Last Name" className='form--item' >
-          {getFieldDecorator('last', {
-            rules: [
-              {
-                type: 'string',
-                message: 'The Last Name should be string!',
-              },
-              {
-                whitespace: true,
-                message: 'Delete the spaces!'
-              },
-              {
-                min: 3,
-                message: 'Last Name must be 3 charcter at least!',
-              },
-              {
-                required: true,
-                message: 'Please inter your Last Name!',
-              },
-            ],
-          })(<Input placeholder='Your Last Name' />)}
-        </Form.Item>
-
-        <Form.Item label="Email" className='form--item' >
-          {getFieldDecorator('email', {
-            rules: [
-              {
-                type: 'email',
-                message: 'The input is not valid E-mail!',
-              },
-              {
-                required: true,
-                message: 'Please input your E-mail!',
-              },
-            ],
-          })(<Input placeholder='Email' />)}
-        </Form.Item>
-
-        <Form.Item label="Password" hasFeedback className='form--item' >
-          {getFieldDecorator('password', {
-            rules: [
-              {
-                required: true,
-                message: 'Please input your password!',
-              },
-              {
-                whitespace: true,
-                message: 'Delete the spaces!'
-              },
-              {
-                validator: this.validateToNextPassword,
-              },
-              {
-                min: 8,
-                message: 'Password must be 8 charcter at least!',
-              }
-            ],
-          })(<Input.Password placeholder='Password' />)}
-        </Form.Item>
-
-        <Form.Item label="Confirm Password" hasFeedback className='form--item' >
-          {getFieldDecorator('confirm', {
-            rules: [
-              {
-                required: true,
-                message: 'Please confirm your password!',
-              },
-              {
-                validator: this.compareToFirstPassword,
-              },
-            ],
-          })(<Input.Password onBlur={this.handleConfirmBlur} placeholder='Confirm Password' />)}
-        </Form.Item>
-        <div className='form-div'>
-          <Form.Item  >
-            <Button type="submit" className='form--btn-save' onClick={() => null} >
-              Next
-          </Button>
-            <Button className='form--btn-cancel' onClick={this.handleCancel}>
-              Cancel
-          </Button>
+          <Form.Item
+            label="First Name"
+            className='form--item'
+          >
+            {getFieldDecorator('first_name', {
+              rules: [
+                {
+                  type: 'string',
+                  message: 'The First Name should be string!',
+                },
+                {
+                  whitespace: true,
+                  message: 'Delete the spaces!'
+                },
+                {
+                  min: 3,
+                  message: 'First Name must be 3 charcter at least!',
+                },
+                {
+                  required: true,
+                  message: 'Please inter your First Name!',
+                },
+              ],
+            })(
+              <Input
+                placeholder='Your First Name'
+              />)}
           </Form.Item>
-        </div>
 
+          <Form.Item
+            label="Last Name"
+            className='form--item'
+          >
+            {getFieldDecorator('last_name', {
+              rules: [
+                {
+                  type: 'string',
+                  message: 'The Last Name should be string!',
+                },
+                {
+                  whitespace: true,
+                  message: 'Delete the spaces!'
+                },
+                {
+                  min: 3,
+                  message: 'Last Name must be 3 charcter at least!',
+                },
+                {
+                  required: true,
+                  message: 'Please inter your Last Name!',
+                },
+              ],
+            })(
+              <Input
+                placeholder='Your Last Name'
+              />)}
+          </Form.Item>
+
+          <Form.Item
+            label="Email"
+            className='form--item'
+          >
+            {getFieldDecorator('email', {
+              rules: [
+                {
+                  type: 'email',
+                  message: 'The input is not valid E-mail!',
+                },
+                {
+                  required: true,
+                  message: 'Please input your E-mail!',
+                },
+              ],
+            })(
+              <Input
+                placeholder='Email'
+              />)}
+          </Form.Item>
+
+          <Form.Item
+            label="Password"
+            hasFeedback
+            className='form--item'
+          >
+            {getFieldDecorator('password', {
+              rules: [
+                {
+                  required: true,
+                  message: 'Please input your password!',
+                },
+                {
+                  whitespace: true,
+                  message: 'Delete the spaces!'
+                },
+                {
+                  validator: this.validateToNextPassword,
+                },
+                {
+                  min: 8,
+                  message: 'Password must be 8 charcter at least!',
+                }
+              ],
+            })(
+              <Input.Password
+                placeholder='Password'
+              />)}
+          </Form.Item>
+
+          <Form.Item
+            label="Confirm Password"
+            hasFeedback
+            className='form--item'
+          >
+            {getFieldDecorator('confirm_password', {
+              rules: [
+                {
+                  required: true,
+                  message: 'Please confirm your password!',
+                },
+                {
+                  validator: this.compareToFirstPassword,
+                },
+              ],
+            })(
+              <Input.Password
+                onBlur={this.handleConfirmBlur}
+                placeholder='Confirm Password'
+              />)}
+          </Form.Item>
+        </InputGroup>
+
+        <Form.Item  >
+          <Button
+            type="submit"
+            className='form--btn-save'
+            onClick={() => null}
+          >
+            Next
+          </Button>
+          <Button
+            className='form--btn-cancel'
+            onClick={this.handleCancel}
+          >
+            Cancel
+          </Button>
+        </Form.Item>
       </Form>
     );
   }
