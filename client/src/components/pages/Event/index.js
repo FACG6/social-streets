ed509db4ffbf6746c70f2998ced0787397acbcc2
AbdallahAmmar96 <abodsaid1996@gmail.com>
2019-06-02 13:37:24 +0300

    edit the event page

--- client/src/components/pages/Event/index.js
+++ client/src/components/pages/Event/index.js
@@ -1,83 +1,108 @@
-import React from 'react'
+import React, { Component } from 'react'
+import { Icon, Divider } from 'antd'
 import PropTypes from 'prop-types';
-import { Icon } from 'antd'
+
 
 import 'antd/dist/antd.css'
 
 import eventInfo from './staticData'
 import './style.css'
 
-function renderInfo(props) {
-  const keys = Object.keys(props);
-  const elements = keys.map((element, index) => {
-    if (element === 'title' || element === 'image' || element === 'publishDate' || element === 'publisher') return null
-    return (
-      <section key={index} className={'event' + element}>
-        {element === 'description' ? (
-          <>
-            <h2>Event {element.charAt(0).toUpperCase() + element.slice(1)}</h2>
-            <p>{props[element]}</p>
-          </>
-        )
-          : (element === 'organiserWebsite' ? (
-            <>
-              <h2>{element.charAt(0).toUpperCase() + element.slice(1)}</h2>
-              <h5><a href={props[element]}>{props[element]}</a></h5>
-            </>
-          ) : (element === 'cost' ? (
-            <>
-              <h2>{element.charAt(0).toUpperCase() + element.slice(1)}</h2>
-              <h5><Icon type="euro" /> {props[element]}</h5>
-            </>
-          ) : (
-              <>
-                <h2>Event {element.charAt(0).toUpperCase() + element.slice(1)}</h2>
-                <h6>{props[element]}</h6>
-              </>
-            )
-            ))
-        }
-        <hr />
-      </section >
-    )
-  })
-  return elements
-}
+class Event extends Component {
 
-export default function Event(props) {
-  const { image, title, publishDate, publisher, type } = eventInfo
-  return (
-    <div className='event-container'>
-      <section>
-        <img className='event-img' src={image} alt="Event image" />
-        <h1>{title}</h1>
-      </section>
-      <section className='event-flex'>
-        <div className='event-flex--div' >
-          <h5> <Icon type="calendar" style={{ fontSize: '20px' }} /> Posted on {publishDate}</h5>
-          <h5> <Icon type="folder" style={{ fontSize: '20px' }} /> Posted in Events</h5>
-        </div>
-        <div className='event-flex--div' >
-          <h5> <Icon type="user" style={{ fontSize: '20px' }} /> {publisher}</h5>
-          <h5> <Icon type="folder" style={{ fontSize: '20px' }} /> {type}</h5>
-        </div>
-      </section>
-      {renderInfo(eventInfo)}
-    </div>
-  )
+  state = {
+    event: {},
+  }
+
+  componentDidMount() {
+    const { postStatus } = this.props
+    if (postStatus === 'published') {
+      // fetch
+      this.setState({ event: eventInfo })
+    }
+    this.setState({ event: this.props })
+  }
+
+  render() {
+    const { postStatus } = this.props;
+    let eventData
+
+    if (postStatus === 'published') {
+      eventData = this.state.event
+    } else {
+      eventData = this.props
+    }
+
+    const { image, title, publishDate, publisher, type, topic, description, dateTime, venue, organiserWebsite, cost } = eventData
+    const pargraphs = description ? description.split('\n') : null;
+
+    return (
+      <>
+        {!title
+          ? (<h1>Loading ...</h1>)
+          : (
+            <section className='event-container' >
+              <img className='event-img' src={image} alt="Event image" />
+              <h1 className='event-title' >{title}</h1>
+              <div className='event-icon' >
+                <div className='event-icon--col' >
+                  <h5> <Icon type="calendar" style={{ fontSize: '12px', paddingRight: '5px' }} /> Posted on {publishDate}</h5>
+                  <h5> <Icon type="folder" style={{ fontSize: '12px', paddingRight: '5px' }} /> Posted in Events</h5>
+                </div>
+                <div className='event-flex--col' >
+                  <h5> <Icon type="user" style={{ fontSize: '12px', paddingRight: '5px' }} /> {publisher}</h5>
+                  <h5> <Icon type="folder" style={{ fontSize: '12px', paddingRight: '5px' }} /> {type}</h5>
+                </div>
+              </div>
+              <Divider />
+              <div className='event-body' >
+                <h3 className='event--lable'>Event Type</h3>
+                <span>{type}</span>
+                <Divider />
+                <h3 className='event--lable'>Event Topic</h3>
+                <span>{topic.map((tag, index) => {
+                  return (
+                    <span key={index}>{tag} </span>
+                  )
+                })}</span>
+                <Divider />
+                <h3 className='event--lable'>Event Description</h3>
+                <span> {pargraphs.map((paragraph, index) => {
+                  return <p key={index} className='public-service--paragraph'>{paragraph}</p>
+                })}</span>
+                <Divider />
+                <h3 className='event-lable'>Event Date & Time</h3>
+                <span>{dateTime}</span>
+                <Divider />
+                <h3 className='event--lable'>Event Venue</h3>
+                <span>{venue}</span>
+                <Divider />
+                <h3 className='event--lable'>Organizer Website</h3>
+                <span><a href={organiserWebsite} style={{ color: '#e85f5f' }} >{organiserWebsite}</a></span>
+                <Divider />
+                <h3 className='event--lable'>Cost</h3>
+                <span><Icon type="euro" style={{ paddingRight: '5px' }} />{cost}</span>
+              </div>
+            </section>
+          )}
+      </>
+    )
+  }
 }
 
 Event.propTypes = {
-  title: String.isRequired(),
-  type: String.isRequired(),
-  topic: String.isRequired(),
-  description: String.isRequired(),
-  dateTime: String.dateTime.isRequired(),
-  venue: String.isRequired(),
-  organiserWebsite: String.link.isRequired(),
-  cost: Number.isRequired(),
-  image: String.file.isRequired(),
-  publishDate: String.date.isRequired(),
-  publisher: String.isRequired(),
-};
+  postStatus: PropTypes.string.isRequired,
+  image: PropTypes.string,
+  title: PropTypes.string,
+  publishDate: PropTypes.string,
+  publisher: PropTypes.string,
+  type: PropTypes.string,
+  topic: PropTypes.arrayOf(PropTypes.string),
+  description: PropTypes.string,
+  dateTime: PropTypes.string,
+  venue: PropTypes.string,
+  organizationWebsite: PropTypes.string,
+  cost: PropTypes.number,
+}
 
+export default Event
