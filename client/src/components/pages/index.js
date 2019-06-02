39d8c25f11c528ed7451a207ac5705bd161da91d
aminalakhsham <aminking140@gmail.com>
2019-06-02 11:37:45 +0300

    add all pages in index file

--- client/src/components/pages/index.js
+++ client/src/components/pages/index.js
@@ -1,4 +1,6 @@
 import CreatPost from "components/pages/CreatePost";
-import Home from "components/pages/homepage";
+import Home from "components/pages/Home";
 import PageNotFound from "components/pages/PageNotFound";
-export { CreatPost, Home, PageNotFound };
+import Login from "components/pages/Login";
+import createProfile from "components/pages/createProfile";
+export { CreatPost, Home, PageNotFound, Login, createProfile };
