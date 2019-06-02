08937d5666b61dcd6eb4f6a60a912a10a10874eb
AbdallahAmmar96 <abodsaid1996@gmail.com>
2019-06-02 13:37:43 +0300

    edit the topic

--- client/src/components/pages/Event/staticData.js
+++ client/src/components/pages/Event/staticData.js
@@ -4,8 +4,10 @@ const eventInfo = {
   publishDate: 'April, 11 2019',
   publisher: 'Gaza Sky Geeks',
   type: 'Community Content',
-  topic: 'Heritage and Culture',
-  description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.',
+  topic: ['Heritage', 'Culture'],
+  description: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+  Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+  Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.`,
   dateTime: '11/6/2019 / 04.30 p.m',
   venue: 'Abed al Aziz Venue',
   organiserWebsite: 'www.gaza-sky-geeks.com',
