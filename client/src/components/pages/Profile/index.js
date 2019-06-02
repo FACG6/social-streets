32702bb8e96258a1cec527eac63033ad3f770ceb
Ahmed I. Abdellatif <ahmedisam9922@gmail.com>
2019-06-02 11:52:13 +0300

    add prop types for the login page component

--- client/src/components/pages/Profile/index.js
+++ client/src/components/pages/Profile/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { Tabs, Icon } from "antd";
-import PropTypes from "prop-types";
+import PropTypes, { func } from "prop-types";
 
 import PersonalInfoForm from "components/utils/ProfilePersonal";
 import BusinessForm from "components/utils/businessForm";
@@ -8,34 +8,36 @@ import "./style.css";
 
 const { TabPane } = Tabs;
 
-export default class ProfilePage extends Component {
-  render() {
-    const { personal, business } = this.props;
-    return (
-      <Tabs defaultActiveKey="1" animated={true}>
-        <TabPane
-          tab={
-            <span>
-              <Icon type="user" />
-              Personal Info
-            </span>
-          }
-          key="1"
-        >
-          <PersonalInfoForm personal={personal} />
-        </TabPane>
-        <TabPane
-          tab={
-            <span>
-              <Icon type="project" />
-              Business
-            </span>
-          }
-          key="2"
-        >
-          <BusinessForm business={business} />
-        </TabPane>
-      </Tabs>
-    );
-  }
+export default function ProfilePage({ personal, business }) {
+  return (
+    <Tabs defaultActiveKey="1" animated={true}>
+      <TabPane
+        tab={
+          <span>
+            <Icon type="user" />
+            Personal Info
+          </span>
+        }
+        key="1"
+      >
+        <PersonalInfoForm personal={personal} />
+      </TabPane>
+      <TabPane
+        tab={
+          <span>
+            <Icon type="project" />
+            Business
+          </span>
+        }
+        key="2"
+      >
+        <BusinessForm business={business} />
+      </TabPane>
+    </Tabs>
+  );
 }
+
+ProfilePage.propTypes = {
+  personal: PropTypes.object.isRequired,
+  business: PropTypes.object.isRequired
+};
