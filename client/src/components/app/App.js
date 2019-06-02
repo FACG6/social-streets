63c1a32bcd3166f128f2c8fd732287e85cfee429
Israa <israa96sulaiman@gmail.com>
2019-06-02 12:39:00 +0300

    remove app from PR

--- client/src/components/app/App.js
+++ client/src/components/app/App.js
@@ -2,12 +2,9 @@ import React from "react";
 import "antd/dist/antd.css";
 
 import "./App.css";
-import Header from 'components/utils/Header';
-import Post from 'components/pages/Post'
-import Footer from 'components/utils/footer';
 
 function App() {
-  return <><Header /> <Post postType='draft' /> <Footer /></>;
+  return <></>;
 }
 
 export default App;
