faa9e3faa6698179c46c82950e0401929e10bd44
aminalakhsham <aminking140@gmail.com>
2019-06-02 14:22:39 +0300

    add components to index file

--- client/src/components/pages/index.js
+++ client/src/components/pages/index.js
@@ -1,6 +1,7 @@
 import CreatPost from "components/pages/CreatePost";
 import Home from "components/pages/Home";
+import Header from "components/utils/Header";
 import PageNotFound from "components/pages/PageNotFound";
 import Login from "components/pages/Login";
 import createProfile from "components/pages/createProfile";
-export { CreatPost, Home, PageNotFound, Login, createProfile };
+export { CreatPost, Home, PageNotFound, Login, createProfile, Header };
