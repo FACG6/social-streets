e2210b71eb15702eb21efe3dbe194b5e1164d0dc
Israa <israa96sulaiman@gmail.com>
2019-06-03 13:38:41 +0300

    remove fragments

--- client/src/components/app/App.js
+++ client/src/components/app/App.js
@@ -18,79 +18,77 @@ import "./App.css";
 
 function App() {
   return (
-    <>
-      <Router>
-        <Header />
-        <Switch>
-          <Route
-            exact
-            path='/'
-            component={Home}
-          />
-          <Route
-            path='/signup'
-            component={CreateProfile}
-          />
-          <Route
-            path='/login'
-            component={Login}
-          />
-          {/* <Route
+    <Router>
+      <Header />
+      <Switch>
+        <Route
+          exact
+          path='/'
+          component={Home}
+        />
+        <Route
+          path='/signup'
+          component={CreateProfile}
+        />
+        <Route
+          path='/login'
+          component={Login}
+        />
+        {/* <Route
             exact
             path='/profile/:id'
             component={Profile}
           /> */}
-          <Route
-            path='/posts/live'
-            render={(props) => <Post {...props} postType='live' />}
-          />
-          <Route
-            path='/posts/draft'
-            render={(props) => <Post {...props} postType='draft' />}
-          />
-          <Route
-            path='/posts/new'
-            component={PostForm}
-          />
-          <Route
-            exact
-            path='/post/public-service/:id/edit'
-            render={(props) => <PostForm postFormType='public service' {...props} />}
-          />
-          <Route
-            exact
-            path='/post/event/:id/edit'
-            render={(props) => <PostForm postFormType='event' {...props} />}
-          />
-          <Route
-            path='/post/event/:category/:id'
-            render={(props) => <Event {...props} postStatus='published' />}
-          />
-          <Route
-            path='/post/public-service/:category/:id'
-            render={(props) => <PublicService {...props} postStatus='published' />}
-          />
-          <Route
-            path='/post/event/:category/:id/preview'
-            render={(props) => <Event {...props} postStatus='published' />}
-          />
-          <Route
-            path='/post/public-service/:category/:id/preview'
-            render={(props) => <PublicService {...props} postStatus='published' />}
-          />
-          <Route
-            path='/posts'
-            component={Posts}
-          />
-          <Route
-            path='/logout'
-            render={() => <h1>Logout</h1>}
-          />
-          <Route component={PageNotFound} />
-        </Switch>
-        <Footer />
-      </Router>
-    </>
+        <Route
+          path='/posts/live'
+          render={(props) => <Post {...props} postType='live' />}
+        />
+        <Route
+          path='/posts/draft'
+          render={(props) => <Post {...props} postType='draft' />}
+        />
+        <Route
+          path='/posts/new'
+          component={PostForm}
+        />
+        <Route
+          exact
+          path='/post/public-service/:id/edit'
+          render={(props) => <PostForm postFormType='public service' {...props} />}
+        />
+        <Route
+          exact
+          path='/post/event/:id/edit'
+          render={(props) => <PostForm postFormType='event' {...props} />}
+        />
+        <Route
+          path='/post/event/:category/:id'
+          render={(props) => <Event {...props} postStatus='published' />}
+        />
+        <Route
+          path='/post/public-service/:category/:id'
+          render={(props) => <PublicService {...props} postStatus='published' />}
+        />
+        <Route
+          path='/post/event/:category/:id/preview'
+          render={(props) => <Event {...props} postStatus='published' />}
+        />
+        <Route
+          path='/post/public-service/:category/:id/preview'
+          render={(props) => <PublicService {...props} postStatus='published' />}
+        />
+        <Route
+          path='/posts'
+          component={Posts}
+        />
+        <Route
+          path='/logout'
+          render={() => <h1>Logout</h1>}
+        />
+        <Route component={PageNotFound} />
+      </Switch>
+      <Footer />
+    </Router>
   )
 }
 
