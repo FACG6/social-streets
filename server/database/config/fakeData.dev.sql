ad77393f28e03a4f7f3b06c864f966f6db1ef52f
Ahmed I. Abdellatif <ahmedisam9922@gmail.com>
2019-06-12 15:36:07 +0300

    update table names

--- server/database/config/fakeData.dev.sql
+++ server/database/config/fakeData.dev.sql
@@ -1,15 +1,15 @@
 BEGIN;
 
-    INSERT INTO event_categories (category) 
+    INSERT INTO event_category (category) 
         VALUES ("Events and Festivals"), ("Nightlife"), ("Exhibitions"), ("Courses and workshops"), ("Walks and talks"); 
 
-    INSERT INTO topics (topic) 
+    INSERT INTO topic (topic) 
         VALUES ("Family and Children"), ("Food and drink"), ("Arts"), ("Heritage and Culture"), ("Sport and Hobbies"), ("Community Development");
 
-    INSERT INTO primary_tags (tag) 
+    INSERT INTO primary_tag (tag) 
         VALUES ("Consultations"), ("Petitions"),("Surveys"), ("Volunteering"), ("Research");
 
-    INSERT INTO secondary_tags (tag) 
+    INSERT INTO secondary_tag (tag) 
         VALUES ("Health"), ("Safety"),("Planning"), ("Education");
 
 COMMIT;
