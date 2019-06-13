954a0202ab3ffeacc3a284eba1a3cea7031244e7
Khader <kakastarcris@gmail.com>
2019-06-13 16:19:50 +0300

    add onFinish

--- server/test/index.js
+++ server/test/index.js
@@ -1,24 +1,8 @@
 const tape = require('tape');
-const { updatePasswordQuery } = require('./../database/queries/updatePassword');
-const { buildDb, buildFakeData, buildStaticData } = require('./../database/config/build');
 
 tape('test', (e) => {
   e.equal(1, 1, 'pass');
   e.end();
 });
 
-
-tape('Update user password', (e) => {
-  const email = 'ahmedisam9922@gmail.com';
-  const password = '123456';
-  buildDb()
-    .then(() => buildStaticData())
-    .then(() => buildFakeData())
-    .then(() => {
-      updatePasswordQuery(email, password)
-        .then((result) => {
-          console.log(result);
-        });
-    });
-  e.end();
-});
+tape.onFinish(() => process.exit(0));
