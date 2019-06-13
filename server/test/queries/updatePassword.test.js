5b307b2335405e929ff9535119f3fabaa1507c9b
Khader <kakastarcris@gmail.com>
2019-06-13 15:03:33 +0300

    ss

--- server/test/queries/updatePassword.test.js
+++ server/test/queries/updatePassword.test.js
@@ -1,14 +1,9 @@
 const tape = require('tape');
-const { updatePasswordQuery } = require('./../database/queries/updatePassword');
-const { buildDb, buildFakeData, buildStaticData } = require('./../database/config/build');
 
-tape('test', (e) => {
-  e.equal(1, 1, 'pass');
-  e.end();
-});
+const { updatePasswordQuery } = require('../../database/queries/updatePassword');
+const { buildDb, buildFakeData, buildStaticData } = require('../../database/config/build');
 
-
-tape('Update user password', (e) => {
+tape('Update user password', (t) => {
   const email = 'ahmedisam9922@gmail.com';
   const password = '123456';
   buildDb()
@@ -20,5 +15,4 @@ tape('Update user password', (e) => {
           console.log(result);
         });
     });
-  e.end();
 });
