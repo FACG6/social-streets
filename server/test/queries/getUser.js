6b6d073959d338acc0763c389a33a510a1d526ee
Ahmed I. Abdellatif <ahmedisam9922@gmail.com>
2019-06-13 15:52:07 +0300

    Add tests for the login route Relates #74

--- server/test/queries/getUser.js
+++ server/test/queries/getUser.js
@@ -8,14 +8,13 @@ test('Testing getUser Query', (t) => {
     .then(() => buildStaticData())
     .then(() => buildFakeData())
     .then(() => getUser('ahmedisam9922@gmail.com'))
-    .then((res) => {
-      const user = res.rows[0];
+    .then((user) => {
       t.equal(typeof user, 'object', 'should be object');
       t.equal(Object.keys(user).length >= 11, true, 'should be equal to or more than 11 keys');
       t.equal(Object.keys(user).includes('first_name'), true, 'user must contain first_name');
       t.equal(user.first_name, 'Ahmed', 'First name must be Ahmed');
       t.equal(user.last_name, 'Abdellatif', 'Last name must be Abdellatif');
-      t.equal(user.address, 'Omar Al-Mukhtar', 'Adress must be Omar Al-Mukhtar');
+      t.equal(user.address, 'Omar Al-Mukhtar St.', 'Adress must be Omar Al-Mukhtar');
       t.end();
     });
 });
---
+++ server/test/routes/login.js
@@ -0,0 +1,44 @@
+const test = require('tape');
+const supertest = require('supertest');
+
+const app = require('../../app');
+
+test('Testing login route', (t) => {
+  supertest(app)
+    .post('/api/v1/login')
+    .send({ email: 'ahmedisam9922@gmail.com', password: '123' })
+    .expect(200)
+    .expect('Content-Type', 'application/json; charset=utf-8')
+    .end((err, res) => {
+      if (err) t.error(err);
+      const resUser = JSON.parse(res.text);
+      t.equal(resUser.first_name, 'Ahmed', 'First name must be Ahmed');
+      t.equal(resUser.last_name, 'Abdellatif', 'Last name must be Abdellatif');
+      t.equal(resUser.address, 'Omar Al-Mukhtar St.', 'Address must be Omar Al-Mukhtar St.');
+    });
+
+  supertest(app)
+    .post('/api/v1/login')
+    .send({ email: 'ahmedisam9922@gmail.com', password: '124' })
+    .expect(400)
+    .expect('Content-Type', 'text/html; charset=utf-8')
+    .end((err, res) => {
+      if (err) t.error(err);
+      t.equal(res.text, 'Wrong password', 'expect password to be wrong');
+    });
+
+  supertest(app)
+    .post('/api/v1/login')
+    .send({ email: 'ahmed.isam@gmail.com', password: '123' })
+    .expect(400)
+    .expect('Content-Type', 'text/html; charset=utf-8')
+    .end((err, res) => {
+      if (err) t.error(err);
+      t.equal(
+        res.text,
+        "User with email 'ahmed.isam@gmail.com' does not exist",
+        'expect email to be wrong',
+      );
+      t.end();
+    });
+});
