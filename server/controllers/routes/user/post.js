cd689e4aa3ebdd37628bd35be8d7ff55fbc1efdb
aminalakhsham <aminking140@gmail.com>
2019-06-13 16:07:07 +0300

    add hash password function

--- server/controllers/routes/user/post.js
+++ server/controllers/routes/user/post.js
@@ -1,25 +1,31 @@
 /* eslint-disable camelcase */
 const { insertUser } = require('./../../../database/queries/insertUser');
 const { userPostSchema } = require('./../../utils/validationSchemes');
+const { hashPassword } = require('./../../utils/helper');
 
 exports.post = (req, res, next) => {
   const userInfo = { ...req.body.user };
   userInfo.avatar = 'avatar.png';
-
   userPostSchema
     .validate(userInfo)
     .then((valid) => {
-      if (valid) return insertUser(userInfo);
+      if (valid) return hashPassword(userInfo.password, 10);
       return res.status(400).send({
         error: 'bad request',
         statusCode: 400,
       });
     })
+    .then((hashedPass) => {
+      userInfo.password = hashedPass;
+      return insertUser(userInfo);
+    })
     .then((result) => {
+      const { password, ...userInfoResult } = result.rows[0];
       res.status(201).send({
-        data: { ...result.rows[0] },
+        data: { ...userInfoResult },
         statusCode: 201,
       });
     })
     .catch(next);
 };
+insertUser(userInfo);
