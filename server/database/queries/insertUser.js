4a250816bfa52fd2c07ab14e37afb80e92766a2d
aminalakhsham <aminking140@gmail.com>
2019-06-13 14:11:43 +0300

    fix query

--- server/database/queries/insertUser.js
+++ server/database/queries/insertUser.js
@@ -5,4 +5,4 @@ exports.insertUser = userInfo => connect.query(`INSERT INTO
                                                   (first_name,last_name,email,password,business_type,website,organisation_name,address,city,country,zip_code,facebook,twitter,instagram,avatar) 
                                                 VALUES
                                                   ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13,$14,$15)
-                                                RETURNING * `, Object.values(userInfo));
+                                                RETURNING *`, Object.values(userInfo));
