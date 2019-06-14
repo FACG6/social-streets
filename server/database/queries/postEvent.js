2b6822a0d9840eadc47157ae046fd85f15423368
AbdallahAmmar96 <abodsaid1996@gmail.com>
2019-06-15 02:30:24 +0300

    edit the spaces and lines

--- server/database/queries/postEvent.js
+++ server/database/queries/postEvent.js
@@ -1,58 +1,106 @@
 const connection = require('../config/connection');
 
 addEvent = ({
-      title,
-      description,
-      category,
-      image,
-      event_datetime,
-      venue,
-      website,
-      cost,
-      focus_key,
-      meta,
-      alt_text,
-      is_draft,
-      publisher_id,
-      publish_datetime
+	title,
+	description,
+	category,
+	image,
+	event_datetime,
+	venue,
+	website,
+	cost,
+	focus_key,
+	meta,
+	alt_text,
+	is_draft,
+	publisher_id,
+	publish_datetime
 }) => connection.query(`
-INSERT INTO event
-      (title, description, category, event_datetime, venue, website, image, cost, focus_key, meta, alt_text, is_draft, publisher_id, publish_datetime)
-VALUES 
-      ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14) RETURNING *;`, [title, description, category, event_datetime, venue, website, image, cost, focus_key, meta, alt_text, is_draft, publisher_id, publish_datetime])
+	INSERT INTO event
+		(title, description, category, event_datetime, venue, website, image, cost, focus_key, meta, alt_text, is_draft, publisher_id, publish_datetime)
+	VALUES 
+    ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14) 
+  RETURNING *;`,
+	[
+		title,
+		description,
+		category,
+		event_datetime,
+		venue,
+		website,
+		image,
+		cost,
+		focus_key,
+		meta,
+		alt_text,
+		is_draft,
+		publisher_id,
+		publish_datetime
+	]
+);
 
 addTopic = (event_id, topic_id) => connection.query(`
-INSERT INTO event_topic
-      (event_id, topic_id)
-VALUES 
-      ($1, $2) RETURNING *;`, [event_id, topic_id])
+	INSERT INTO event_topic
+  	(event_id, topic_id)
+	VALUES 
+		($1, $2)
+	RETURNING *`,
+	[
+		event_id,
+		topic_id
+	]
+);
+
 
 addPublicServices = ({
-      primary_tag,
-      description,
-      image,
-      focus_key,
-      alt_text,
-      meta,
-      publisher_id,
-      publish_datetime,
-      title,
-      is_draft
+	primary_tag,
+	description,
+	image,
+	focus_key,
+	alt_text,
+	meta,
+	publisher_id,
+	publish_datetime,
+	title,
+	is_draft
 }) => connection.query(`
   INSERT INTO public_services
-        (primary_tag, description, image, focus_key, alt_text, meta, publisher_id, publish_datetime, title, is_draft)
+    (primary_tag, description, image, focus_key, alt_text, meta, publisher_id, publish_datetime, title, is_draft)
   VALUES 
-        ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10) RETURNING *;`, [primary_tag, description, image, focus_key, alt_text, meta, publisher_id, publish_datetime, title, is_draft])
+		($1, $2, $3, $4, $5, $6, $7, $8, $9, $10) 
+	RETURNING *`,
+	[
+		primary_tag,
+		description,
+		image,
+		focus_key,
+		alt_text,
+		meta,
+		publisher_id,
+		publish_datetime,
+		title,
+		is_draft
+	]
+);
+
+
 
 
 addSecondaryTag = (public_services_id, secondary_tag) => connection.query(`
-  INSERT INTO public_services_tag (public_services_id, secondary_tag)
-  VALUES
-      ($1, $2) RETURNING *;`, [public_services_id, secondary_tag])
+	INSERT INTO public_services_tag
+		(public_services_id, secondary_tag)
+	VALUES
+		($1, $2) 
+	RETURNING *`,
+	[
+		public_services_id,
+		secondary_tag
+	]
+);
 
 module.exports = {
-      addEvent,
-      addTopic,
-      addPublicServices,
-      addSecondaryTag
-}
+	addEvent,
+	addTopic,
+	addPublicServices,
+	addSecondaryTag
+};
