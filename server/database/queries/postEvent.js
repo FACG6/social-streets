b23950549b43251696d1f8f9303ebc907c7a00d9
AbdallahAmmar96 <abodsaid1996@gmail.com>
2019-06-15 01:34:21 +0300

    add the queries to insert a post

--- server/database/queries/postEvent.js
+++ server/database/queries/postEvent.js
@@ -1,8 +1,36 @@
 const connection = require('../config/connection');
 
-module.exports = ({title, description, category, event_datetime, venue, website, cost, image, focus_key, meta, alt_text, is_draft, publisher_id, publish_datetime}) => connection.query(`
+addEvent = ( {title, description, category, image, event_datetime, venue, website, cost, focus_key, meta, alt_text, is_draft, publisher_id, publish_datetime} ) => {
+  return connection.query(`
 INSERT INTO event
-(title, description, category, event_datetime, venue, website, cost, image, focus_key, meta, alt_text, is_draft, publisher_id, publish_datetime)
-VALUES ('The main Event', ' Lorem Ipsum has been the industrys standard dummy text ever since the 1500s,', 1, '11/6/ 2019 - 04.30 p.m', 'Abed al Aziz Venue',  'www.gaza-sky-geeks.com', '15', 'https://images.pexels.com/photos/617278/pexels-photo-617278.jpeg?auto=compress& cs=tinysrgb&dpr=1&w=500', 'Lorem', 'Lorem  Ipsum is simply dummy text of the printing  and typesetting industry.', 'alt text', false, 2, '11/4/2019 - 02.23 p.m')
-returning *`,
-[title, description, category, event_datetime, venue, website, cost, image, focus_key, meta, alt_text, is_draft, publisher_id, publish_datetime]);
+      (title, description, category, event_datetime, venue, website, image, cost, focus_key, meta, alt_text, is_draft, publisher_id, publish_datetime)
+VALUES 
+      ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14) RETURNING *;`, [ title, description, category, event_datetime, venue,  website, image, cost, focus_key, meta, alt_text, is_draft, publisher_id, publish_datetime ]
+);}
+
+addTopic = (event_id, topicArray) => {
+  connection.query(`
+INSERT INTO event_topic
+      (event_id, topic_id)
+VALUES 
+      ($1, $2) RETURNING *;`, [event_id, topicArray]
+);}
+
+addPublicServices = ({primary_tag, description, image, focus_key, alt_text, meta, publisher_id, publish_datetime, title, is_draft}) => {
+  return connection.query(`
+  INSERT INTO public_services
+        (primary_tag, description, image, focus_key, alt_text, meta, publisher_id, publish_datetime, title, is_draft)
+  VALUES 
+        ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10) RETURNING *;`, [primary_tag, description, image, focus_key, alt_text, meta, publisher_id, publish_datetime, title, is_draft]
+  ) 
+}
+
+addSecondaryTag = (public_services_id, secondary_tag) => {
+   return connection.query(`
+  INSERT INTO public_services_tag (public_services_id, secondary_tag)
+  VALUES
+      ($1, $2) RETURNING *;`, [public_services_id, secondary_tag]
+)
+}
+
+module.exports = { addEvent, addTopic, addPublicServices, addSecondaryTag }
