70d243cbca318ca8cc2e7a6936c54c1f18ffd76c
AbdallahAmmar96 <abodsaid1996@gmail.com>
2019-06-15 01:37:34 +0300

    add the test for the route /post

--- server/test/routes/post/post.js
+++ server/test/routes/post/post.js
@@ -1,39 +1,82 @@
 const test = require('tape');
 const request = require('supertest');
-const app = require('../../../app');
-const dbBuild = require('../../../database/config/build');
+const app = require('../app');
+const {
+  buildDb,
+  buildFakeData,
+  buildStaticData
+} = require('../database/config/build');
 
 test('Add new post at /api/v1/post/', (t) => {
-  dbBuild()
+  buildDb()
+    .then(() => buildStaticData())
+    .then(() => buildFakeData())
     .then(() => {
       request(app)
         .post('/api/v1/post/')
-        .send({
-          firstname: 'firstname',
-          lastname: 'lastname',
-          phonenumber: '12345678',
-          mobilenumber: '123456789',
-          address: 'address',
-          level: 5,
-          password: 'password',
-        })
+        .set('Cookie', ['jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiaWF0IjoxNTYwNDE5NDE2fQ.MCP5Rx0eu31Hjyb2gL9YXd9n5w7SHTwOMjjHNNgeovM'])
+        .field('type', 'event')
+        .field('title', 'Title from')
+        .field('description', 'description for the event')
+        .field('category', 1)
+        .field('event_datetime', '11/4/2019 - 02.23 p.m')
+        .field('venue', 'gaza st')
+        .field('website', 'www.qqqq.com')
+        .field('alt_text', 'desc fro img')
+        .field('cost', 15)
+        .field('is_draft', false)
+        .field('focus_key', 'focusKeyword')
+        .field('meta', 'this is metaDescription')
+        .field('publish_datetime', '11/4/2019')
+        .field('eventTopic', [1, 2])
+        .attach('eventImg', 'test/amideasblue.png')
         .expect(201)
         .expect('Content-Type', /json/)
         .end((err, res) => {
-          const student = (res.body.data);
           if (err) {
             t.error(err);
           } else {
-            t.equal(student.password, 'password', 'Same password');
-            t.equal(student.mobile_phone, '123456789', 'Same mobile_phone');
-            t.equal(student.lastname, 'lastname', 'Same lastname');
-            t.equal(student.home_phone, '12345678', 'Same home_phone');
-            t.equal(student.level, 5, 'Same level');
-            t.equal(student.address, 'address', 'Same address');
-            t.equal(student.firstname, 'firstname', 'Same firstname');
+            t.deepEqual(Object.keys(res.body.data), ['id', 'title', 'description', 'category', 'event_datetime', 'venue', 'website', 'cost', 'image', 'focus_key', 'meta', 'alt_text', 'is_draft', 'publisher_id', 'publish_datetime'], 'Event add sucssfully')
+            t.equal(res.body.data.id, 2, 'Same id for the new Event Post')
             t.end();
           }
         });
     })
     .catch(err => t.error(err));
 });
+
+test('Add new post at /api/v1/post/', (t) => {
+  buildDb()
+    .then(() => buildStaticData())
+    .then(() => buildFakeData())
+    .then(() => {
+      request(app)
+        .post('/api/v1/post/')
+        .set('Cookie', ['jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiaWF0IjoxNTYwNDE5NDE2fQ.MCP5Rx0eu31Hjyb2gL9YXd9n5w7SHTwOMjjHNNgeovM'])
+        .field('type', 'public_services')
+        .field('primary_tag', 1)
+        .field('description', 'description for the public_services')
+        .field('focus_key', 'focusKeyword')
+        .field('alt_text', 'desc fro img')
+        .field('meta', 'this is metaDescription')
+        .field('publish_datetime', '11/4/2019')
+        .field('title', 'Title public_services')
+        .field('is_draft', false)
+        .field('secondary_tag', [1, 2, 3])
+        .attach('publicImg', 'test/amideasblue.png')
+        .expect(201)
+        .expect('Content-Type', /json/)
+        .end((err, res) => {
+          if (err) {
+            t.error(err);
+          } else {
+            t.deepEqual(Object.keys(res.body.data), ['id', 'primary_tag', 'description', 'image', 'focus_key', 'alt_text', 'meta', 'publisher_id', 'publish_datetime', 'title', 'is_draft', ], 'Public Services added sucssfully')
+            t.equal(res.body.data.id, 2, 'Same id for the new Public Services Post')
+            t.end();
+          }
+        });
+    })
+    .catch(err => t.error(err));
+});
+
+test.onFinish(() => process.exit(0));
