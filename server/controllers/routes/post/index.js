c1a5741e11f1e58e1d0b4e792c58924b3588ad04
AbdallahAmmar96 <abodsaid1996@gmail.com>
2019-06-15 18:12:27 +0300

    edit the route to handle the get method

--- server/controllers/routes/post/index.js
+++ server/controllers/routes/post/index.js
@@ -1,9 +1,11 @@
 const router = require('express').Router();
 
 const addPost = require('./addPost');
+const draftPosts = require('./getDraftPosts')
 
 router
   .route('/')
+  .get(draftPosts)
   .post(addPost);
 
 module.exports = router;
