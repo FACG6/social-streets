46205a9a6ba56c0ba7a39f84699f47c0db8d8c3c
AbdallahAmmar96 <abodsaid1996@gmail.com>
2019-06-15 18:14:06 +0300

    test the get draft event

---
+++ server/test/queries/getDraftEvents.js
@@ -0,0 +1,30 @@
+const tape = require('tape');
+
+const { buildDb, buildFakeData, buildStaticData } = require('./../../database/config/build.js');
+const { getDraftEvents } = require('../../database/queries/getDraftPosts');
+
+tape('Get draft Events', async (t) => {
+  try {
+    await buildDb()
+    await buildStaticData()
+    await buildFakeData()
+    const publisher_id = 2;
+    const res = await getDraftEvents(publisher_id)
+    if (res.rowCount !== 0) {
+      t.deepEqual(
+        Object.keys(res.rows[0]),
+        [ 'id', 'category' ],
+        'Get all draft events'
+      );  
+      t.equal(res.rows[0].id, 1, 'Same id for the event');
+      t.end();
+    } else {
+      t.equal(res.rowCount, 0, 'No draft event in the database')
+      t.end();
+    }  
+  } catch (err) {
+    t.error(err);
+  }  
+});
+
+tape.onFinish(() => process.exit(0));
