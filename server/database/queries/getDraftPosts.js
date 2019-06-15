6ec5e59b4f396ad5053c82a539e6817471e8695b
AbdallahAmmar96 <abodsaid1996@gmail.com>
2019-06-15 18:13:40 +0300

    add the query to get the draft posts

---
+++ server/database/queries/getDraftPosts.js
@@ -0,0 +1,19 @@
+const connection = require('../config/connection');
+
+getDraftEvents = (publisher_id) => connection.query(`
+  SELECT id, category
+  from event
+  WHERE  is_draft = true and publisher_id = $1
+`,
+  [publisher_id]
+);
+
+getDraftPublicServices = (publisher_id) => connection.query(`
+  SELECT id, primary_tag
+  from public_services
+  WHERE  is_draft = true and publisher_id = $1
+`,
+  [publisher_id]
+);
+
+module.exports = { getDraftEvents, getDraftPublicServices };
