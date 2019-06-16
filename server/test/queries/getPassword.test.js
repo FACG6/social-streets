14bc7d2731e9ca8d3e36834c8a3b31c0a4ef732e
Khader <kakastarcris@gmail.com>
2019-06-16 12:20:13 +0300

    test for getPassword query

---
+++ server/test/queries/getPassword.test.js
@@ -0,0 +1,24 @@
+const tape = require('tape');
+
+const { getPassword } = require('./../../database/queries/updatePassword');
+const { buildDb, buildFakeData, buildStaticData } = require('./../../database/config/build');
+
+tape('get password', (e) => {
+  const id = 1;
+  buildDb()
+    .then(() => buildStaticData())
+    .then(() => buildFakeData())
+    .then(() => {
+      getPassword(id)
+        .then((result) => {
+          if (result.rowCount === 1) {
+            e.deepEqual(result.rowCount, 1, 'get password');
+            e.end();
+          } else {
+            e.error();
+          }
+        });
+    });
+});
+
+tape.onFinish(() => process.exit(0));
