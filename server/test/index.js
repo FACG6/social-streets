bb567a2c3e88aca492b7567d1a618004cff694d6
Khader <kakastarcris@gmail.com>
2019-06-16 13:11:46 +0300

    solve conflict in tests

--- server/test/index.js
+++ server/test/index.js
@@ -4,13 +4,3 @@ tape('test', (e) => {
   e.equal(1, 1, 'pass');
   e.end();
 });
-
-require('./queries/getPassword.test');
-require('./queries/insertEvent');
-require('./queries/insertPublicServices');
-require('./queries/insertSecondaryTag');
-require('./queries/insertTopic');
-require('./queries/insertUser.test');
-require('./queries/updatePassword.test');
-require('./routes/passwordPut.test');
-require('./routes/post/post');
