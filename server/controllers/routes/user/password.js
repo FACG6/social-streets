c418532dde60a0b7facb774964ac076ca81e78b0
Khader <kakastarcris@gmail.com>
2019-06-16 14:50:09 +0300

    modify

--- server/controllers/routes/user/password.js
+++ server/controllers/routes/user/password.js
@@ -1,30 +1,38 @@
-const { compare } = require('bcryptjs');
+const { compare, compareSync } = require("bcryptjs");
 
-const hashPassword = require('../../utils/hashPassword');
-const passwordSchema = require('./../../utils/passwordSchema');
-const { updatePasswordQuery } = require('./../../../database/queries/updatePassword').updatePasswordQuery;
-const { getPassword } = require('./../../../database/queries/getPassword').getPassword;
+const { hashPassword } = require("../../utils/hashPassword");
+const passwordSchema = require("./../../utils/passwordSchema");
+const {
+  updatePasswordQuery
+} = require("./../../../database/queries/updatePassword");
+const { getPassword } = require("./../../../database/queries/getPassword");
 
 exports.updatePassword = (req, res) => {
   const { oldPassword, newPassword } = req.body;
   getPassword(req.user.id)
     .then(dbRes => compare(oldPassword, dbRes.rows[0].password))
-    .then(() => {
-      passwordSchema.isValid(newPassword);
-    })
-    .then((valid) => {
-      if (valid) return hashPassword(newPassword);
+    .then(passMatch => {
+      if (passMatch) return passwordSchema.isValid({ password: newPassword });
       return res.status(400).send({
-        error: 'Bad Request!',
-        statusCode: 400,
+        error: "Wrong Password",
+        statusCode: 400
       });
     })
-    .then(updatePasswordQuery)
-    .then(() => res.send({ data: 'Updated Password Successfully', statusCode: 200 }))
-    .catch((e) => {
+    .then(valid => {
+      if (valid) return hashPassword(newPassword);
+      return res.status(400).send({
+        error: "Bad Request!",
+        statusCode: 400
+      });
+    })
+    .then(newHashedPass => updatePasswordQuery(newHashedPass, req.user.id))
+    .then(() =>
+      res.send({ data: "Updated Password Successfully", statusCode: 200 })
+    )
+    .catch(e => {
       res.status(500).send({
-        error: 'Internal Server Error',
-        statusCode: 500,
+        error: "Internal Server Error",
+        statusCode: 500
       });
     });
 };
