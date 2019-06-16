9565293cb19fb4104737739cd904f7e0a188150e
Khader <kakastarcris@gmail.com>
2019-06-16 14:50:48 +0300

    modify test

--- server/test/routes/passwordPut.test.js
+++ server/test/routes/passwordPut.test.js
@@ -19,14 +19,14 @@ test('PUT in /api/v1/user/password', (t) => {
           'jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiaWF0IjoxNTYwNDE5NDE2fQ.MCP5Rx0eu31Hjyb2gL9YXd9n5w7SHTwOMjjHNNgeovM',
         ])
         .send({
-          oldPassword: '$2y$12$0tqIpfxzTSFIKFECzjS1XOKhhxAlPsexglTCOKhysSXVt.R4KTBAW',
-          newPassword: '123',
+          oldPassword: '123',
+          newPassword: '123456789',
         })
         .expect(200)
         .expect('Content-Type', /json/)
         .end((err, res) => {
           if (err) t.error(err);
-          t.deepEqual(res.body.data, 'Updated Password Successfully', 'Updated');
+          t.equal(JSON.parse(res.text).data, 'Updated Password Successfully', 'Updated');
           t.end();
         });
     })
