20233709fff537f37eddc05b20d6f43e96ae6f25
Khader <kakastarcris@gmail.com>
2019-06-16 12:30:46 +0300

    modify import

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,9 +1,10 @@
 const router = require('express').Router();
 
 const { user } = require('./routes');
+const unlockedCookie = require('./middlewares/unlockCookie');
 
 
-router.use(require('./middlewares/unlockCookie'));
+router.use(unlockedCookie);
 
 router.use('/user', user);
 
