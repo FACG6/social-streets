d2fdd0b401ce62e7133448dcc3d49cf60729f61f
aminalakhsham <aminking140@gmail.com>
2019-06-16 13:35:01 +0300

    fix error

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,7 +1,11 @@
 const router = require('express').Router();
 
 const { user, post } = require('./routes');
+const { post: userPost } = require('./routes/user/post');
+const unlockedCookie = require('./middlewares/unlockCookie');
 
+router.post('/user',userPost);
+router.use(unlockedCookie);
 router.use('/user', user);
 router.use('/post', post);
 
--- server/controllers/routes/post/get.js
+++ server/controllers/routes/post/get.js
@@ -1,30 +1,33 @@
-const { getAuthPost } = require('../../../database/queries/getAuthPost');
-const { getEvent, getPublicService } = require('./../../../database/queries/getPost');
-const { fetchPostSchema } = require('./../../utils/validationSchemes');
+const { getAuthPost } = require("../../../database/queries/getAuthPost");
+const {
+  getEvent,
+  getPublicService
+} = require("./../../../database/queries/getPost");
+const { fetchPostSchema } = require("./../../utils/validationSchemes");
 
 exports.get = (req, res, next) => {
   const { id: idUser } = req.user;
   const { postId } = req.params;
   const { postType } = req.body;
   fetchPostSchema
-    .validate({ postId, postType })
-    .then((valid) => {
+    .isValid({ postId, postType, idUser })
+    .then(valid => {
       if (valid) return getAuthPost(postType, postId, idUser);
       return res.status(400).send({
-        error: 'bad request',
-        statusCode: 400,
+        error: "bad request",
+        statusCode: 400
       });
     })
-    .then((authRes) => {
+    .then(authRes => {
       if (authRes.rowCount === 1) {
-        if (postType === 'event') {
+        if (postType === "event") {
           return getEvent(postId);
         }
-        if (postType === 'public_service') {
+        if (postType === "public_service") {
           return getPublicService(postId);
         }
       }
-      return res.status(401).send({ error: 'unauthorized' });
+      return res.status(401).send({ error: "unauthorized" });
     })
     .then(result => res.send({ data: result.rows, statusCode: 200 }))
     .catch(err => next(err));
