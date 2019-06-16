90ebe5ec554dbe6f79c3b6f95b650da248fd72e0
Khader <kakastarcris@gmail.com>
2019-06-16 12:39:08 +0300

    require all tests

--- server/test/index.js
+++ server/test/index.js
@@ -1,3 +1,10 @@
+const tape = require('tape');
+
+tape('test', (e) => {
+  e.equal(1, 1, 'pass');
+  e.end();
+});
+
 require('./queries/getPassword.test');
 require('./queries/updatePassword.test');
 require('./routes/passwordPut.test');
