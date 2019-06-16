8b9e189740afdce4f3883a8733658f6d3f5ef0fe
Khader <kakastarcris@gmail.com>
2019-06-16 12:13:20 +0300

    add unlockCookie middleware

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -2,6 +2,9 @@ const router = require('express').Router();
 
 const { user } = require('./routes');
 
+
+router.use(require('./middlewares/unlockCookie'));
+
 router.use('/user', user);
 
 module.exports = router;
