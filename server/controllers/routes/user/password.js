226138ddef36ae7ea9d02878f3b4f08790ffd653
Khader <kakastarcris@gmail.com>
2019-06-16 12:16:27 +0300

    Add password controller

--- server/controllers/routes/user/password.js
+++ server/controllers/routes/user/password.js
@@ -1,9 +1,30 @@
-const yup = require('yup');
+const { compare } = require('bcryptjs');
 
-const updatePasswordQuery = require('./../../../database/queries/updatePassword');
+const hashPassword = require('../../utils/hashPassword');
+const passwordSchema = require('./../../utils/passwordSchema');
+const { updatePasswordQuery } = require('./../../../database/queries/updatePassword').updatePasswordQuery;
+const getPassword = require('./../../../database/queries/getPassword');
 
-exports.updatePassword = (req, res, next) => {
-  const schema = yup.object().shape({
-    password: yup.string().min(8),
-  });
+exports.updatePassword = (req, res) => {
+  const { oldPassword, newPassword } = req.body;
+  getPassword(req.user.id)
+    .then(dbRes => compare(oldPassword, dbRes.rows[0].password))
+    .then(() => {
+      passwordSchema.isValid(newPassword);
+    })
+    .then((valid) => {
+      if (valid) return hashPassword(newPassword);
+      return res.status(400).send({
+        error: 'Bad Request!',
+        statusCode: 400,
+      });
+    })
+    .then(updatePasswordQuery)
+    .then(() => res.send({ data: 'Updated Password Successfully', statusCode: 200 }))
+    .catch((e) => {
+      res.status(500).send({
+        error: 'Internal Server Error',
+        statusCode: 500,
+      });
+    });
 };
