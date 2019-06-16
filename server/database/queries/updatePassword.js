08fb24d25f50b9199b591a2ec4f797489be6fa32
Khader <kakastarcris@gmail.com>
2019-06-16 15:24:34 +0300

    update query

--- server/database/queries/updatePassword.js
+++ server/database/queries/updatePassword.js
@@ -1,7 +1,8 @@
 const connection = require("./../config/connection");
 
 exports.updatePasswordQuery = (newPassword, userId) =>
-  connection.query('UPDATE "user" SET password = $1 WHERE id = $2', [
+  connection.query('UPDATE "user" SET password = $1 WHERE id = $2 RETURNING true', [
     newPassword,
     userId
   ]);
+  
