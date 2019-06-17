f2bc0b116956bf01471de5f8a5220692a5cc6272
aminalakhsham <aminking140@gmail.com>
2019-06-17 15:07:50 +0300

    fix conflict

--- client/src/components/pages/createProfile/index.js
+++ client/src/components/pages/createProfile/index.js
@@ -1,57 +1,96 @@
-import React, { Component } from 'react'
-import { Steps } from 'antd';
+import React, { Component } from "react";
+import { Steps, notification } from "antd";
+import axios from "axios";
 
-import PersonalProfile from 'components/utils/personalForm'
-import BusinessProfile from 'components/utils/businessForm'
-import './style.css'
+import PersonalProfile from "components/utils/personalForm";
+import BusinessProfile from "components/utils/businessForm";
+import "./style.css";
 
 const { Step } = Steps;
 
 export default class Profile extends Component {
-
   state = {
     personal: {},
     business: {},
-    current: 0,
-  }
+    current: 0
+  };
 
   handlePersonalInfo = (data, e) => {
-    e.preventDefault()
-    this.setState({ personal: data, current: 1 })
-  }
+    e.preventDefault();
+    this.setState({ personal: data, current: 1 });
+  };
 
   handleSubmit = (data, e) => {
-    e.preventDefault()
-    this.setState({ business: data })
-    setTimeout(() => console.log(this.state), 1000)
-  }
+    e.preventDefault();
+    this.setState({ business: data }, () => {
+      const {
+        personal: { confirm_password, ...restPersonal },
+        business
+      } = this.state;
+      axios
+        .post("/api/v1/user", { ...restPersonal, ...business })
+        .then(() => this.props.history.push("/login"))
+        .catch(err => {
+          const errObj = {
+            message: "Error",
+            description: "There is Error please try again"
+          };
+          if (err.status === 500) {
+            errObj.message = "Server Error";
+            errObj.description =
+              "Internal Server Error, Please try again later";
+          } else {
+            errObj.message = "Validation Error";
+            errObj.description = "Please, Check the data you entered";
+          }
+          const { message, description } = errObj;
+          notification.open({
+            message,
+            description
+          });
+        });
+    });
+  };
 
   handleGoBack = (data, e) => {
-    e.preventDefault()
-    this.setState({ current: 0, business: data })
-  }
+    e.preventDefault();
+    this.setState({ current: 0, business: data });
+  };
 
   render() {
-    const { current, personal, business } = this.state
+    const { current, personal, business } = this.state;
     return (
       <div>
-        <div className='create-profile--header'>
+        <div className="create-profile--header">
           <h1>Create New Profile</h1>
-          {current === 0
-            ? <p>Complete this step to provide us with some personal information</p>
-            : <p>Complete this step to provide us with some information about your organization</p>
-          }
+          {current === 0 ? (
+            <p>
+              Complete this step to provide us with some personal information
+            </p>
+          ) : (
+            <p>
+              Complete this step to provide us with some information about your
+              organization
+            </p>
+          )}
         </div>
-        <Steps size="small" current={current} labelPlacement='vertical'>
+        <Steps size="small" current={current} labelPlacement="vertical">
           <Step />
           <Step />
         </Steps>
-        {
-          current
-            ? <BusinessProfile handleSubmit={this.handleSubmit} business={business} handleGoBack={this.handleGoBack} />
-            : <PersonalProfile handlePersonalInfo={this.handlePersonalInfo} personal={personal} />
-        }
+        {current ? (
+          <BusinessProfile
+            handleSubmit={this.handleSubmit}
+            business={business}
+            handleGoBack={this.handleGoBack}
+          />
+        ) : (
+          <PersonalProfile
+            handlePersonalInfo={this.handlePersonalInfo}
+            personal={personal}
+          />
+        )}
       </div>
-    )
+    );
   }
 }
