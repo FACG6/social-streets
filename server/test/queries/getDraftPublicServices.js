91993fea8b8d07cb0b2a81ef0b8895cdfc4fcec6
AbdallahAmmar96 <abodsaid1996@gmail.com>
2019-06-17 09:14:55 +0300

    edit the query

--- server/test/queries/getDraftPublicServices.js
+++ server/test/queries/getDraftPublicServices.js
@@ -9,7 +9,7 @@ tape('Get draft Events', async (t) => {
     await buildStaticData()
     await buildFakeData()
     const publisherId = 1;
-    const res = await getPublicServices(true, publisherId)
+    const res = await getPublicServices('true', publisherId)
     if (res.rowCount !== 0) {
       t.deepEqual(
         Object.keys(res.rows[0]),
