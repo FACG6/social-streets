d2f6a2a9a0502c9f3ff7b1620dd9d787f40cfbce
Ahmed I. Abdellatif <ahmedisam9922@gmail.com>
2019-06-17 14:04:41 +0300

    remove notRequired restriction

--- server/controllers/utils/validationSchemes.js
+++ server/controllers/utils/validationSchemes.js
@@ -45,14 +45,12 @@ exports.userPostSchema = yup.object().shape({
     .string()
     .matches(
       /^((https?|ftp|smtp):\/\/)?(www.)?[a-z0-9]+(\.[a-z]{2,}){1,3}(#?\/?[a-zA-Z0-9#]+)*\/?(\?[a-zA-Z0-9-_]+=[a-zA-Z0-9-%]+&?)?$/
-    )
-    .notRequired(),
+    ),
   twitter: yup
     .string()
     .matches(
       /^((https?|ftp|smtp):\/\/)?(www.)?[a-z0-9]+(\.[a-z]{2,}){1,3}(#?\/?[a-zA-Z0-9#]+)*\/?(\?[a-zA-Z0-9-_]+=[a-zA-Z0-9-%]+&?)?$/
-    )
-    .notRequired(),
+    ),
   instagram: yup
     .string()
     .matches(
