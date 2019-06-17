3a264faf39ab474b03c95e57a543034db3d17e7a
Ahmed I. Abdellatif <ahmedisam9922@gmail.com>
2019-06-17 11:33:03 +0300

    testing

--- server/controllers/routes/login.js
+++ server/controllers/routes/login.js
@@ -1,36 +1,50 @@
-const bcrypt = require('bcryptjs');
+const bcrypt = require("bcryptjs");
 
-const { genCookie } = require('../utils/helper.js');
-const getUser = require('../../database/queries/getUser');
-const { loginSchema } = require('../utils/validationSchemes');
+const { genCookie } = require("../utils/helper.js");
+const getUser = require("../../database/queries/getUser");
+const { loginSchema } = require("../utils/validationSchemes");
 
 module.exports = (req, res) => {
+  console.log("HELLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLOOOOOOOOOOOOOOO");
   const { email, password } = req.body;
 
   loginSchema
     .isValid({ email, password })
-    .then((valid) => {
-      if (!valid) return res.status(400).send({ error: 'Bad Request', statusCode: 400 });
+    .then(valid => {
+      if (!valid)
+        return res.status(400).send({ error: "Bad Request", statusCode: 400 });
       return getUser(email);
     })
-    .then((user) => {
+    .then(user => {
       if (!user) {
         res
           .status(400)
-          .send({ error: `User with email '${email}' does not exist`, statusCode: 400 });
+          .send({
+            error: `User with email '${email}' does not exist`,
+            statusCode: 400
+          });
       } else {
         bcrypt
           .compare(password, user.password)
-          .then((passIsValid) => {
-            if (!passIsValid) res.status(400).send({ error: 'Wrong password', statusCode: 400 });
+          .then(passIsValid => {
+            if (!passIsValid)
+              res
+                .status(400)
+                .send({ error: "Wrong password", statusCode: 400 });
             else {
               const { password: pass, ...userResult } = user;
-              res.cookie('jwt', genCookie(user));
+              res.cookie("jwt", genCookie(user));
               res.send({ data: userResult, statusCode: 200 });
             }
           })
-          .catch(() => res.status(500).send({ error: 'Internal Server Error', statusCode: 500 }));
+          .catch(() =>
+            res
+              .status(500)
+              .send({ error: "Internal Server Error", statusCode: 500 })
+          );
       }
     })
-    .catch(() => res.status(500).send({ error: 'Internal Server Error', statusCode: 500 }));
+    .catch(() =>
+      res.status(500).send({ error: "Internal Server Error", statusCode: 500 })
+    );
 };
