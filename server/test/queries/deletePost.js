a8cea2d3724066a1dd92d83216607b38cba4089c
Israa <israa96sulaiman@gmail.com>
2019-06-17 13:58:18 +0300

    edit test

--- server/test/queries/deletePost.js
+++ server/test/queries/deletePost.js
@@ -4,14 +4,11 @@ const connection = require('../../database/config/connection');
 const { deleteEvent, deletePublicService } = require('./../../database/queries/deletePost');
 const { buildDb, buildFakeData, buildStaticData } = require('./../../database/config/build.js');
 
-const eventId = () => connection.query('SELECT id from event limit 1');
-
 test('testing delete Event Query', (t) => {
   buildDb()
     .then(buildStaticData)
     .then(buildFakeData)
-    .then(eventId)
-    .then(id => deleteEvent(id.rows[0].id))
+    .then(() => deleteEvent(1))
     .then((response) => {
       t.equal(Object.keys(response.rows[0]).length, 15, 'should be 15 keys');
       t.equal(Object.keys(response.rows[0]).includes('id'), true, 'should include id of event');
@@ -24,10 +21,9 @@ test('testing delete Event Query', (t) => {
 
 test('testing delete public service Query', (t) => {
   buildDb()
-    .then(() => buildStaticData())
-    .then(() => buildFakeData())
-    .then(() => eventId())
-    .then(id => deletePublicService(id.rows[0].id))
+    .then(buildStaticData)
+    .then(buildFakeData)
+    .then(() => deletePublicService(1))
     .then((response) => {
       const keys = ['id', 'primary_tag', 'description', 'image', 'focus_key', 'alt_text', 'meta', 'publisher_id', 'publish_datetime', 'title', 'is_draft']
       t.equal(Object.keys(response.rows[0]).length, 11, 'should be 11 keys');
