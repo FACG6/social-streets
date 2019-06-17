85477469eba0249384629e96d872a7c8e03ee6b9
Israa <israa96sulaiman@gmail.com>
2019-06-17 23:22:34 +0300

    remove unnecessary file

--- server/database/queries/postEvent.js
+++ server/database/queries/postEvent.js
@@ -1,54 +1,54 @@
 const connection = require('../config/connection');
 
 const addEvent = ({
-      title,
-      description,
-      category,
-      imageName,
-      eventDatetime,
-      venue,
-      website,
-      cost,
-      focusKey,
-      meta,
-      altText,
-      isDraft,
-      publisherId,
-      publishDatetime
+		      title,
+          description,
+          category,
+          imageName,
+          eventDatetime,
+          venue,
+          website,
+          cost,
+          focusKey,
+          meta,
+          altText,
+          isDraft,
+          publisherId,
+          publishDatetime
 }) => connection.query(`
 	INSERT INTO event
 		(title, description, category, event_datetime, venue, website, image, cost, focus_key, meta, alt_text, is_draft, publisher_id, publish_datetime)
 	VALUES 
     ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14) 
   RETURNING *;`,
-      [
-        title,
-        description,
-        category,
-        eventDatetime,
-        venue,
-        website,
-        imageName,
-        cost,
-        focusKey,
-        meta,
-        altText,
-        isDraft,
-        publisherId,
-        publishDatetime
-      ]
+        [
+              title,
+              description,
+              category,
+              eventDatetime,
+              venue,
+              website,
+              imageName,
+              cost,
+              focusKey,
+              meta,
+              altText,
+              isDraft,
+              publisherId,
+              publishDatetime
+        ]
   );
 
 const addTopic = (eventId, topicId) => connection.query(`
-      INSERT INTO event_topic
+        INSERT INTO event_topic
         (event_id, topic_id)
-      VALUES 
-        ($1, $2)
-      RETURNING *`,
-      [
-        eventId,
-        topicId
-      ]
+        VALUES 
+              ($1, $2)
+        RETURNING *`,
+        [
+              eventId,
+              topicId
+        ]
 );
 
 const addPublicServices = ({
@@ -66,31 +66,31 @@ const addPublicServices = ({
   INSERT INTO public_service
     (primary_tag, description, image, focus_key, alt_text, meta, publisher_id, publish_datetime, title, is_draft)
   VALUES 
-        ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10) 
+              ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10) 
       RETURNING *`,
       [
-        primaryTag,
-        description,
-        imageName,
-        focusKey,
-        altText,
-        meta,
-        publisherId,
-        publishDatetime,
-        title,
-        isDraft
+              primaryTag,
+              description,
+              imageName,
+              focusKey,
+              altText,
+              meta,
+              publisherId,
+              publishDatetime,
+              title,
+              isDraft
       ]
 );
 
 const addSecondaryTag = (publicServiceId, secondaryTag) => connection.query(`
       INSERT INTO public_service_tag
-        (public_service_id, secondary_tag)
+              (public_service_id, secondary_tag)
       VALUES
-        ($1, $2) 
+              ($1, $2) 
       RETURNING *`,
       [
-        publicServiceId,
-        secondaryTag
+              publicServiceId,
+              secondaryTag
       ]
 );
 
