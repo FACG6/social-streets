f5090c09cb24ef003a70a4747576da27e0fa5da3
Israa <israa96sulaiman@gmail.com>
2019-06-17 16:32:45 +0300

    remove line

--- server/controllers/routes/user/get.js
+++ server/controllers/routes/user/get.js
@@ -3,11 +3,9 @@ const { getUserById } = require('./../../../database/queries/getUser');
 exports.get = (req, res, next) => {
   getUserById(req.user.id)
     .then((response) => {
-      if (!response.rowCount)
-        return res.status(400).send({ error: 'Bad Request', statusCode: 400 });
+      if (!response.rowCount) { return res.status(400).send({ error: 'Bad Request', statusCode: 400 }); }
       delete response.rows[0].password;
-      res.send({ data: response.rows[0], statusCode: 200 });
-
+      return res.send({ data: response.rows[0], statusCode: 200 });
     })
     .catch(next);
 };
