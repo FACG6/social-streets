366ffe6c5a4f437e3c619755ccb56ef36daabbcd
Israa <israa96sulaiman@gmail.com>
2019-06-17 10:55:04 +0300

    edit test

--- server/test/queries/getLivePublicService.js
+++ server/test/queries/getLivePublicService.js
@@ -3,6 +3,8 @@ const tape = require('tape');
 const { buildDb, buildFakeData, buildStaticData } = require('./../../database/config/build.js');
 const { getPublicServices } = require('../../database/queries/getPosts');
 
+const livePosts = [{ id: 1, primary_tag: 2, title: 'News Title' }];
+
 tape('Get Live Public Services | Query', async (t) => {
   try {
     await buildDb()
@@ -12,6 +14,7 @@ tape('Get Live Public Services | Query', async (t) => {
     const res = await getPublicServices('false', publisherId)
     t.equal(res.rows.length, 1, 'length is 1')
     t.deepEqual(Object.keys(res.rows[0]), ['id', 'primary_tag', 'title'], 'should include id, primary tag and title');
+    t.deepEqual(res.rows, livePosts, 'should be equal');
     t.end();
   } catch (err) {
     t.error(err);
