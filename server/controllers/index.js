0775e3d8ed4f2864d6a134db8ea61cca82fa31f5
Israa <israa96sulaiman@gmail.com>
2019-06-17 15:22:08 +0300

    remove lines

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -4,10 +4,9 @@ const { user, post, login } = require('./routes');
 const { post: userPost } = require('./routes/user/post');
 const unlockCookie = require('./middlewares/unlockCookie');
 
-router.post('/user',userPost);
 router.post('/login', login);
+router.post('/user',userPost);
 router.use(unlockCookie);
-
 router.use('/user', user);
 router.use('/post', post);
 
