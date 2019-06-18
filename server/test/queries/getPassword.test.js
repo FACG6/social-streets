a47dcf29192544fa4e3ec8356cc894bafaa5cc61
Khader <kakastarcris@gmail.com>
2019-06-18 15:58:36 +0300

    test invalid

--- server/test/queries/getPassword.test.js
+++ server/test/queries/getPassword.test.js
@@ -3,20 +3,30 @@ const tape = require('tape');
 const { getPassword } = require('./../../database/queries/getPassword');
 const { buildDb, buildFakeData, buildStaticData } = require('./../../database/config/build');
 
-tape('get password', (e) => {
+tape('Query - Get User Password', (e) => {
   const id = 1;
   buildDb()
-    .then(() => buildStaticData())
-    .then(() => buildFakeData())
+    .then(buildStaticData)
+    .then(buildFakeData)
     .then(() => {
       getPassword(id)
         .then((result) => {
-          if (result.rowCount === 1) {
-            e.deepEqual(result.rowCount, 1, 'get password');
-            e.end();
-          } else {
-            e.error();
-          }
+          e.equal(result.rows[0].password, '$2y$12$0tqIpfxzTSFIKFECzjS1XOKhhxAlPsexglTCOKhysSXVt.R4KTBAW', 'Password Has Gotten');
+          e.end();
+        });
+    });
+});
+
+tape('Query - Get User Password || No id', (e) => {
+  const id = 50;
+  buildDb()
+    .then(buildStaticData)
+    .then(buildFakeData)
+    .then(() => {
+      getPassword(id)
+        .then((result) => {
+          e.equal(result.rowCount, 0, 'No Password');
+          e.end();
         });
     });
 });
