105b2be26106aefd9f10fbfc3ad014f869755e31
Israa <israa96sulaiman@gmail.com>
2019-06-18 22:47:41 +0300

    add post type: event or public service to each post in the backend

--- server/controllers/routes/post/getDraftPosts.js
+++ server/controllers/routes/post/getDraftPosts.js
@@ -3,8 +3,10 @@ const { getEvents, getPublicServices } = require('../../../database/queries/getP
 const draftPosts = async (req, res, next) => {
   try {
     const { id: publisherId } = req.user;
-    const resEvent = await getEvents('true', publisherId)
-    const resPublic = await getPublicServices('true', publisherId)
+    const resEvent = await getEvents('true', publisherId);
+    const resPublic = await getPublicServices('true', publisherId);
+    resEvent.rows.forEach(event => event.type = 'event');
+    resPublic.rows.forEach(publicPost => publicPost.type = 'public_services');
     res.status(200).send({
       data: [...resEvent.rows, ...resPublic.rows],
       statusCode: 200,
