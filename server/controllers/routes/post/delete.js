addc6c6b6188e30f277299096573823e9f27eb51
Israa <israa96sulaiman@gmail.com>
2019-06-18 13:57:18 +0300

    throw error

--- server/controllers/routes/post/delete.js
+++ server/controllers/routes/post/delete.js
@@ -1,25 +1,33 @@
 const { deleteEvent, deletePublicService } = require('../../../database/queries/deletePost');
 
 module.exports = (req, res, next) => {
-  const postId = req.params.postId;
+  const { postId } = req.params;
   const userId = req.user.id;
   const { type } = req.body;
   let callback;
   if (type === 'event') {
     callback = deleteEvent;
   } else if (type === 'public_services') {
-    callback = deletePublicService
+    callback = deletePublicService;
   } else {
-    return res.status(400).send({ error: 'Bad Request', statusCode: 400 })
+    throw Error('Bad Request');
   }
   callback(postId, userId)
     .then((response) => {
       if (!response.rowCount) {
-        res.status(400).send({ statusCode: 400, error: 'Bad Request' });
+        throw Error('Bad Request');
       } else {
         res.send({ statusCode: 200, data: response.rows[0] });
       }
     })
-    .catch(next);
-}
-
+    .catch((err) => {
+      const { message } = err;
+      switch (message) {
+        case 'Bad Request':
+          res.status(400).send({ statusCode: 400, error: message });
+          break;
+        default:
+          next(err);
+      }
+    });
+};
