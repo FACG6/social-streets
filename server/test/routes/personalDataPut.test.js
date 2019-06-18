93c4f8d5413a6fd5916fafb5750bc4556317e284
Khader <kakastarcris@gmail.com>
2019-06-18 15:58:58 +0300

    test invalid

--- server/test/routes/personalDataPut.test.js
+++ server/test/routes/personalDataPut.test.js
@@ -8,10 +8,10 @@ const {
   buildStaticData,
 } = require('./../../database/config/build');
 
-tape('PUT in /api/v1/user/personal', (t) => {
+tape('PUT in /api/v1/user/personal || Valid', (t) => {
   buildDb()
-    .then(() => buildStaticData())
-    .then(() => buildFakeData())
+    .then(buildStaticData)
+    .then(buildFakeData)
     .then(() => {
       requset(app)
         .put('/api/v1/user/personal')
@@ -29,6 +29,60 @@ tape('PUT in /api/v1/user/personal', (t) => {
             t.error(err);
           }
           t.equal(JSON.parse(res.text).data, 'Personal Data Updated Successfully', 'Personal Data Have Updated Successfully');
+          t.end();
+        });
+    })
+    .catch(err => console.log(err));
+});
+
+tape('PUT in /api/v1/user/personal || Invalid Password', (t) => {
+  buildDb()
+    .then(buildStaticData)
+    .then(buildFakeData)
+    .then(() => {
+      requset(app)
+        .put('/api/v1/user/personal')
+        .set('Cookie', ['jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiaWF0IjoxNTYwNDE5NDE2fQ.MCP5Rx0eu31Hjyb2gL9YXd9n5w7SHTwOMjjHNNgeovM'])
+        .send({
+          oldPassword: '********',
+          firstName: 'Khader',
+          lastName: 'Murtaja',
+          email: 'kaka@kaka.com',
+        })
+        .expect(400)
+        .expect('Content-Type', /json/)
+        .end((err, res) => {
+          if (err) {
+            t.error(err);
+          }
+          t.equal(res.body.error, 'Password not match', 'Can not update data with wrong password');
+          t.end();
+        });
+    })
+    .catch(err => console.log(err));
+});
+
+tape('PUT in /api/v1/user/personal || Invalid Data', (t) => {
+  buildDb()
+    .then(buildStaticData)
+    .then(buildFakeData)
+    .then(() => {
+      requset(app)
+        .put('/api/v1/user/personal')
+        .set('Cookie', ['jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiaWF0IjoxNTYwNDE5NDE2fQ.MCP5Rx0eu31Hjyb2gL9YXd9n5w7SHTwOMjjHNNgeovM'])
+        .send({
+          oldPassword: '123',
+          firstName: 'K',
+          lastName: 'M',
+          email: 'com',
+        })
+        .expect(400)
+        .expect('Content-Type', /json/)
+        .end((err, res) => {
+          if (err) {
+            t.error(err);
+          }
+          t.equal(res.body.error, 'Bad Request', 'Can not update data with invalid password');
           t.end();
         });
     })
