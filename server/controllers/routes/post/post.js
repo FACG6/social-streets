e2963244a4c98c566b8316c01768f863cdc782bc
Khader <kakastarcris@gmail.com>
2019-06-18 11:47:45 +0300

    eslint

--- server/controllers/routes/post/post.js
+++ server/controllers/routes/post/post.js
@@ -1,12 +1,12 @@
-const { join } = require("path");
+const { join } = require('path');
 
 const {
   addEvent,
   addTopic,
   addPublicServices,
-  addSecondaryTag
-} = require("../../../database/queries/postEvent");
-const { eventSchema, publicServiceSchema } = require("../../utils/postSchema");
+  addSecondaryTag,
+} = require('../../../database/queries/postEvent');
+const { eventSchema, publicServiceSchema } = require('../../utils/postSchema');
 
 const post = async (req, res, next) => {
   try {
@@ -15,7 +15,7 @@ const post = async (req, res, next) => {
     const { image } = req.files;
     const publisherId = Number(req.user.id);
 
-    if (type === "event") {
+    if (type === 'event') {
       if (!image) throw new Error();
       const valid = await eventSchema.isValid(req.body);
       if (valid) {
@@ -23,75 +23,76 @@ const post = async (req, res, next) => {
         const addedEvent = await addEvent({
           ...req.body,
           publisherId,
-          imageName
+          imageName,
         });
         await Promise.all(
-          eventTopic.map(topicId => addTopic(addedEvent.rows[0].id, topicId))
+          eventTopic.map(topicId => addTopic(addedEvent.rows[0].id, topicId)),
         );
         image.mv(
-          join(__dirname, "..", "..", "..", "uploads", imageName),
-          err => {
+          join(__dirname, '..', '..', '..', 'uploads', imageName),
+          (err) => {
             if (err) {
               next(err);
             } else {
               res.status(201).send({
                 data: {
-                  ...addedEvent.rows[0]
+                  ...addedEvent.rows[0],
                 },
-                statusCode: 201
+                statusCode: 201,
               });
             }
-          }
+          },
         );
       } else {
-        const error = new Error("validation");
+        const error = new Error('validation');
         error.statusCode = 400;
         throw error;
       }
-    } else if (type === "public_services") {
+    } else if (type === 'public_services') {
       const valid = await publicServiceSchema.isValid(req.body);
       if (valid) {
         const imageName = Date.now() + image.name;
         const addedPublicServices = await addPublicServices({
           ...req.body,
           publisherId,
-          imageName
+          imageName,
         });
         await Promise.all(
-          secondaryTag.map(secondaryTagId =>
-            addSecondaryTag(addedPublicServices.rows[0].id, secondaryTagId)
-          )
+          secondaryTag
+            .map(secondaryTagId => addSecondaryTag(addedPublicServices.rows[0].id, secondaryTagId)),
         );
         image.mv(
-          join(__dirname, "..", "..", "..", "uploads", imageName),
-          err => {
+          join(__dirname, '..', '..', '..', 'uploads', imageName),
+          (err) => {
             if (err) {
               next(err);
             } else {
               res.status(201).send({
                 data: {
-                  ...addedPublicServices.rows[0]
+                  ...addedPublicServices.rows[0],
                 },
-                statusCode: 201
+                statusCode: 201,
               });
             }
-          }
+          },
         );
       } else {
-        const error = new Error("validation");
+        const error = new Error('validation');
         error.statusCode = 400;
         throw error;
       }
     } else {
-      const error = new Error("Bad Request");
+      const error = new Error('Bad Request');
       error.statusCode = 400;
       throw error;
     }
   } catch (err) {
-    if (err.statusCode) {
+    const { statusCode, message } = err;
+
+    if (statusCode) {
       res.status(statusCode).send({
-        error: err.message,
-        statusCode: statusCode
+        error: message,
+        statusCode,
       });
     } else {
       next(err);
--- server/database/queries/updatePassword.js
+++ server/database/queries/updatePassword.js
@@ -1,8 +1,6 @@
-const connection = require("./../config/connection");
+const connection = require('./../config/connection');
 
-exports.updatePasswordQuery = (newPassword, userId) =>
-  connection.query('UPDATE "user" SET password = $1 WHERE id = $2 RETURNING true', [
-    newPassword,
-    userId
-  ]);
-  
+exports.updatePasswordQuery = (newPassword, userId) => connection.query('UPDATE "user" SET password = $1 WHERE id = $2 RETURNING true', [
+  newPassword,
+  userId,
+]);
