afc6a25ffbf1459b78a00e45b631ebbb422cce84
aminalakhsham <aminking140@gmail.com>
2019-06-18 08:52:50 +0300

    remove files not relate to PR

--- server/controllers/middlewares/unlockCookie.js
+++ server/controllers/middlewares/unlockCookie.js
@@ -1,18 +1,18 @@
-const { verify } = require('jsonwebtoken');
+const { verify } = require("jsonwebtoken");
 
 module.exports = (req, res, next) => {
   const { jwt } = req.cookies;
   if (jwt) {
     verify(jwt, process.env.SECRET, (error, unlockedCookie) => {
       if (error) {
-        res.clearCookie('jwt');
-        res.status(401).send({ error: 'unauthorized', statusCode: 401 });
+        res.clearCookie("jwt");
+        res.status(401).send({ error: "unauthorized", statusCode: 401 });
       } else {
         req.user = unlockedCookie;
         next();
       }
     });
   } else {
-    res.status(401).send('unauthorized');
+    res.status(401).send("unauthorized");
   }
 };
