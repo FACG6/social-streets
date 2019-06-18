11d7b24fde785e33ad10e18d1966a98bb9ea7b16
Israa <israa96sulaiman@gmail.com>
2019-06-18 21:27:43 +0300

    axios to fetch posts

--- client/src/components/pages/Post/index.js
+++ client/src/components/pages/Post/index.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+import { notification } from 'antd'
 
 import PostButton from 'components/utils/PostButton';
 import PostRow from 'components/utils/PostRow';
 import './style.css';
-import { draft, live } from './postDetails'
 export default class Post extends Component {
   state = {
     posts: [],
@@ -13,21 +14,35 @@ export default class Post extends Component {
   componentDidMount() {
     const { postType } = this.props;
     let allPosts;
-    if (postType === 'draft') {
-      allPosts = draft;
-    } else {
-      allPosts = live;
-    }
-    const posts = allPosts.map(post => {
-      post.link = post.category.toLowerCase().replace(' and ', '-');
-      return post;
-    });
-    this.setState({ posts })
+    axios.get(`/api/v1/post/${postType}`)
+      .then(({ data: { data } }) => {
+        if (!data.length)
+          return this.setState({ error: `No ${postType} Posts Available` });
+        allPosts = data;
+        console.log(allPosts)
+      })
+      .catch(err => {
+        const { statusCode, error } = err;
+        notification.error({
+          message: "ERROR",
+          description: error,
+        })
+        if (statusCode === 401) {
+          this.props.history.push('/login')
+        }
+      })
+
+    // const posts = allPosts.map(post => {
+    //   post.link = post.category.toLowerCase().replace(' and ', '-');
+    //   return post;
+    // });
+    this.setState({ posts: allPosts })
   }
 
   handleDelete = (id) => {
     const { posts } = this.state;
-    //Testing Delete with Mock Data//
+
+    //Testing Delete with Mock Data/
     this.setState({
       posts: posts.filter(post => post.id !== Number(id))
     });
@@ -38,8 +53,8 @@ export default class Post extends Component {
     const { posts, error } = this.state;
     return (
       <section className='post-page--main'>
-        <span className='post-page--error'>{error}</span>
         <PostButton postType={`${postType} Posts`} />
+        <span className='post-page--error'>{error}</span>
         {posts.map(post => <PostRow
           {...post}
           key={post.id}
