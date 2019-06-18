51d8faa4a6be0d0bac3c5716e747a07c90c0a762
Khader <kakastarcris@gmail.com>
2019-06-18 12:59:19 +0300

    Update Personal Data Schema by removing avatar

--- server/controllers/utils/personalSchema.js
+++ server/controllers/utils/personalSchema.js
@@ -1,8 +1,7 @@
 const yup = require('yup');
 
 module.exports = yup.object().shape({
-  first_name: yup.string().min(3).required(),
-  last_name: yup.string().min(3).required(),
+  firstName: yup.string().min(3).required(),
+  lastName: yup.string().min(3).required(),
   email: yup.string().email(),
-  avatar: yup.string(),
 });
