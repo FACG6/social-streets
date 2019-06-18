289c54931f1887c64c7e0a40b65d1af9ac28ec30
Khader <kakastarcris@gmail.com>
2019-06-18 12:58:39 +0300

    Test for updatePersonalDataQuery

--- server/test/queries/updatePersonalDataQuery.test.js
+++ server/test/queries/updatePersonalDataQuery.test.js
@@ -4,26 +4,24 @@ const { updatePersonalDataQuery } = require('./../../database/queries/updatePers
 const { buildDb, buildFakeData, buildStaticData } = require('./../../database/config/build');
 
 tape('Update user personal data', (e) => {
-  const user_id = 1;
-  const first_name = 'aaaa';
-  const last_name = 'bbb';
+  const userId = 1;
+  const firstName = 'Khader';
+  const lastName = 'Murtaja';
   const email = 'aaa@bbb.com';
-  const avatar = 'https://cdn.pixabay.com/photo/2016/03/09/16/50/lake-1246865_960_720.jpg';
   buildDb()
     .then(() => buildStaticData())
     .then(() => buildFakeData())
     .then(() => {
-        updatePersonalDataQuery(first_name, last_name, email, avatar, user_id)
+      updatePersonalDataQuery(firstName, lastName, email, userId)
         .then((result) => {
-          console.log("testtttt");
-        //   if (result.rowCount === 1) {
-        //     e.equal(result.rows[0].bool, true, 'Password Has Changed !!');
-        //     e.end();
-        //   } else {
-        //     e.error();
-        //   }
+          if (result.rowCount === 1) {
+            e.equal(result.rows[0].bool, true, 'Password Has Changed !!');
+            e.end();
+          } else {
+            e.error();
+          }
         });
     });
 });
 
-tape.onFinish(() => process.exit(0)); 
+tape.onFinish(() => process.exit(0));
