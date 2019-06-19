0218d06a0a8ae36fb5d0c8af47befdb919ae73b2
aminalakhsham <aminking140@gmail.com>
2019-06-19 13:15:06 +0300

    fix style

--- client/src/components/app/App.js
+++ client/src/components/app/App.js
@@ -2,12 +2,15 @@ import React from "react";
 
 import "./App.css";
 import { Header, Footer } from "components/utils";
+import { Event } from "components/pages";
 
 function App() {
   return (
     <div>
       <Header />
-      <main className="container" />
+      <main className="container">
+        <Event />
+      </main>
       <Footer />
     </div>
   );
--- client/src/components/pages/Event/index.js
+++ client/src/components/pages/Event/index.js
@@ -1,29 +1,26 @@
-import React, { Component } from 'react'
-import { Icon, Divider } from 'antd'
-import PropTypes from 'prop-types'
+import React, { Component } from "react";
+import { Icon, Divider, Spin } from "antd";
+import PropTypes from "prop-types";
 
-import Button from 'components/utils/Button'
-import './style.css'
+import Button from "components/utils/Button";
+import "./style.css";
 
 class Event extends Component {
+  state = {};
 
-  state = {}
-
-  handleBack = (e) => {
-    e.preventDefault()
-  }
+  handleBack = e => {
+    e.preventDefault();
+  };
 
   componentDidMount() {
-    const { postStatus, ...event } = this.props
-    if (postStatus === 'published') {
+    const { postStatus, ...event } = this.props;
+    if (postStatus === "published") {
       // fetch
-      this.setState({ ...event })
-    } else
-      this.setState({ ...event })
+      this.setState({ ...event });
+    } else this.setState({ ...event });
   }
 
   render() {
-
     const {
       image,
       title,
@@ -36,64 +33,113 @@ class Event extends Component {
       venue,
       organiserWebsite,
       cost
-    } = this.state
-    const { postStatus } = this.state
-    const pargraphs = description ? description.split('\n') : null
+    } = this.state;
+    const { postStatus } = this.state;
+    const pargraphs = description ? description.split("\n") : null;
 
     return (
       <>
-        {!title
-          ? (<h1>Loading ...</h1>)
-          : (
-            <section className='event-container' >
-              <img className='event-img' src={image} alt="Event image" />
-              <h1 className='event-title' >{title}</h1>
-              <div className='event-icon' >
-                <div className='event-icon--col' >
-                  <h5> <Icon type="calendar" style={{ fontSize: '12px', paddingRight: '5px' }} /> Posted on {publishDate}</h5>
-                  <h5> <Icon type="folder" style={{ fontSize: '12px', paddingRight: '5px' }} /> Posted in Events</h5>
-                </div>
-                <div className='event-flex--col' >
-                  <h5> <Icon type="user" style={{ fontSize: '12px', paddingRight: '5px' }} /> {publisher}</h5>
-                  <h5> <Icon type="folder" style={{ fontSize: '12px', paddingRight: '5px' }} /> {type}</h5>
-                </div>
+        {!title ? (
+          <Spin
+            className="event-spin"
+            tip="Loading..."
+            size="large"
+            indicator={<Icon type="loading" style={{ fontSize: 50 }} spin />}
+          />
+        ) : (
+          <section className="event-container">
+            <img className="event-img" src={image} alt="Event image" />
+            <h1 className="event-title">{title}</h1>
+            <div className="event-icon">
+              <div className="event-icon--col">
+                <h5>
+                  {" "}
+                  <Icon
+                    type="calendar"
+                    style={{ fontSize: "12px", paddingRight: "5px" }}
+                  />{" "}
+                  Posted on {publishDate}
+                </h5>
+                <h5>
+                  {" "}
+                  <Icon
+                    type="folder"
+                    style={{ fontSize: "12px", paddingRight: "5px" }}
+                  />{" "}
+                  Posted in Events
+                </h5>
               </div>
+              <div className="event-flex--col">
+                <h5>
+                  {" "}
+                  <Icon
+                    type="user"
+                    style={{ fontSize: "12px", paddingRight: "5px" }}
+                  />{" "}
+                  {publisher}
+                </h5>
+                <h5>
+                  {" "}
+                  <Icon
+                    type="folder"
+                    style={{ fontSize: "12px", paddingRight: "5px" }}
+                  />{" "}
+                  {type}
+                </h5>
+              </div>
+            </div>
+            <Divider />
+            <div className="event-body">
+              <h3 className="event--lable">Event Type</h3>
+              <span>{type}</span>
               <Divider />
-              <div className='event-body' >
-                <h3 className='event--lable'>Event Type</h3>
-                <span>{type}</span>
-                <Divider />
-                <h3 className='event--lable'>Event Topic</h3>
-                <span>{topic.map((tag, index) => {
+              <h3 className="event--lable">Event Topic</h3>
+              <span>
+                {topic.map((tag, index) => {
+                  return <span key={index}>{tag} </span>;
+                })}
+              </span>
+              <Divider />
+              <h3 className="event--lable">Event Description</h3>
+              <span>
+                {" "}
+                {pargraphs.map((paragraph, index) => {
                   return (
-                    <span key={index}>{tag} </span>
-                  )
-                })}</span>
-                <Divider />
-                <h3 className='event--lable'>Event Description</h3>
-                <span> {pargraphs.map((paragraph, index) => {
-                  return <p key={index} className='public-service--paragraph'>{paragraph}</p>
-                })}</span>
-                <Divider />
-                <h3 className='event-lable'>Event Date & Time</h3>
-                <span>{dateTime}</span>
-                <Divider />
-                <h3 className='event--lable'>Event Venue</h3>
-                <span>{venue}</span>
-                <Divider />
-                <h3 className='event--lable'>Organizer Website</h3>
-                <span><a href={organiserWebsite} style={{ color: '#e85f5f' }} >{organiserWebsite}</a></span>
-                <Divider />
-                <h3 className='event--lable'>Cost</h3>
-                <span style={{ paddingBottom: '1rem' }} ><Icon type="euro" />{cost}</span>
-              </div>
-              {!postStatus
-                ? (<Button onClick={this.handleBack} className='event-btn--back' >Back</Button>)
-                : (null)}
-            </section>
-          )}
+                    <p key={index} className="public-service--paragraph">
+                      {paragraph}
+                    </p>
+                  );
+                })}
+              </span>
+              <Divider />
+              <h3 className="event-lable">Event Date & Time</h3>
+              <span>{dateTime}</span>
+              <Divider />
+              <h3 className="event--lable">Event Venue</h3>
+              <span>{venue}</span>
+              <Divider />
+              <h3 className="event--lable">Organizer Website</h3>
+              <span>
+                <a href={organiserWebsite} style={{ color: "#e85f5f" }}>
+                  {organiserWebsite}
+                </a>
+              </span>
+              <Divider />
+              <h3 className="event--lable">Cost</h3>
+              <span style={{ paddingBottom: "1rem" }}>
+                <Icon type="euro" />
+                {cost}
+              </span>
+            </div>
+            {!postStatus ? (
+              <Button onClick={this.handleBack} className="event-btn--back">
+                Back
+              </Button>
+            ) : null}
+          </section>
+        )}
       </>
-    )
+    );
   }
 }
 
@@ -109,7 +155,7 @@ Event.propTypes = {
   dateTime: PropTypes.string,
   venue: PropTypes.string,
   organizationWebsite: PropTypes.string,
-  cost: PropTypes.number,
-}
+  cost: PropTypes.number
+};
 
-export default Event
+export default Event;
