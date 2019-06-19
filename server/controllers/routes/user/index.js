5298dfcd33cd333f3a82097c650b73b3d20b0c83
Khader <kakastarcris@gmail.com>
2019-06-19 16:45:59 +0300

    Add PUT for /business route

--- server/controllers/routes/user/index.js
+++ server/controllers/routes/user/index.js
@@ -2,9 +2,12 @@ const router = require('express').Router();
 
 const { get } = require('./get');
 const { post } = require('./post');
+const { updateBusiness } = require('./business');
 
 router.route('/')
   .get(get)
   .post(post);
+
+router.route('/business').put(updateBusiness);
 
 module.exports = router;
