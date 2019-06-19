0d7be8612673a0598bfd272e3abbddb436d4fe96
Israa <israa96sulaiman@gmail.com>
2019-06-19 16:56:54 +0300

    remove sweet alert2

--- client/src/components/pages/Post/index.js
+++ client/src/components/pages/Post/index.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import { notification } from 'antd';
-import Swal from 'sweetalert2';
 
 import PostButton from 'components/utils/PostButton';
 import PostRow from 'components/utils/PostRow';
