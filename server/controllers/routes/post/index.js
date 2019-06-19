3cbd2b7ab697e29b5adabd8c7c3b0b02402cbeba
aminalakhsham <aminking140@gmail.com>
2019-06-19 17:26:53 +0300

    fix branch

--- server/controllers/routes/post/index.js
+++ server/controllers/routes/post/index.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 
 const draftPosts = require('./getDraftPosts');
+const put = require('./put');
 const livePosts = require('./getLivePosts');
 const deletePost = require('./delete');
 const post = require('./post');
@@ -8,19 +9,20 @@ const { get } = require('./get');
 const eventStatic = require('./eventStatic');
 const publicServiceStatic = require('./publicServiceStatic');
 
-router.route('/').post(post);
+router.post('/', post);
 
 router.get('/draft', draftPosts);
 
 router.get('/live', livePosts);
 
-router
-  .route('/:postId')
-  .get(get)
-  .delete(deletePost);
-
 router.get('/event/static', eventStatic);
 
 router.get('/public-service/static', publicServiceStatic);
+
+router
+  .route('/:postId')
+  .get(get)
+  .put(put)
+  .delete(deletePost);
 
 module.exports = router;
