ea98ea766b04df60998aae2e0f0709ee45438ad4
aminalakhsham <aminking140@gmail.com>
2019-06-19 12:25:31 +0300

    remove unnecessary modules

--- client/src/components/app/App.js
+++ client/src/components/app/App.js
@@ -2,12 +2,15 @@ import React from "react";
 
 import "./App.css";
 import { Header, Footer } from "components/utils";
+import { Login } from "components/pages";
 
 function App() {
   return (
     <div>
       <Header />
-      <main className="container" />
+      <main className="container">
+        <Login/>
+      </main>
       <Footer />
     </div>
   );
