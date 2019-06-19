9c5f23e173f3fc28b40cfd91bccc63be2dfedff5
Khader <kakastarcris@gmail.com>
2019-06-19 16:46:46 +0300

    Build a schema for business data

---
+++ server/controllers/utils/businessSchema.js
@@ -0,0 +1,14 @@
+const yup = require('yup');
+
+exports.businessDataSchema = yup.object().shape({
+  name: yup.string().min(8).required(),
+  type: yup.string().min(8).required(),
+  website: yup.string().url().required(),
+  city: yup.string().min(8).required(),
+  country: yup.string().min(8).required(),
+  address: yup.string().min(15).required(),
+  zipCode: yup.number().min(5).required(),
+  facebook: yup.string().url(),
+  twitter: yup.string().url(),
+  instagram: yup.string().url(),
+});
