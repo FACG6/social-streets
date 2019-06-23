b97919bd0a79247e2f3600b1f345cb804c8e303e
knanahassouna1 <knanahassouna2@gmail.com>
2019-06-23 16:45:38 +0300

    fixed a bug in rendering layout's side

--- client/src/components/auth/protectedRoute.js
+++ client/src/components/auth/protectedRoute.js
@@ -3,18 +3,10 @@ import { Redirect, Route } from "react-router-dom";
 import AdminLayout from "components/utils/AdminLayout";
 
 export default function protectedRoute({ isAuth, isAdmin, user, ...props }) {
-  const { component: Component } = props;
   return isAuth ? (
     isAdmin ? (
       user.role === "admin" ? (
-        <Route
-          {...props}
-          component={props => (
-            <AdminLayout {...props}>
-              <Component {...props} />
-            </AdminLayout>
-          )}
-        />
+        <Route {...props} />
       ) : (
         <Redirect to="/page-not-found" />
       )
