f6ec7394574aa58e84e1f89e71e8bf05eba28b2a
knanahassouna1 <knanahassouna2@gmail.com>
2019-06-23 13:31:52 +0300

    edit routes to include admin pages

--- client/src/components/app/App.js
+++ client/src/components/app/App.js
@@ -3,7 +3,7 @@ import {
   BrowserRouter as Router,
   Switch,
   Route,
-  Redirect,
+  Redirect
 } from "react-router-dom";
 import { notification } from "antd";
 import axios from "axios";
@@ -26,13 +26,17 @@ import ProtectedRoute from "./../auth/protectedRoute";
 import "./App.css";
 
 class App extends Component {
-  state = { user: {}, isAuth: false, isLoading: true };
+  state = { user: { role: "" }, isAuth: false, isLoading: true };
 
   componentDidMount = async () => {
     try {
       const user = (await axios.get("/api/v1/isAuth")).data;
       notification.success({ message: "Welcome Back" });
-      this.setState({ isAuth: true, user, isLoading: false });
+      this.setState({
+        isAuth: true,
+        user: { ...user, role: "member" },
+        isLoading: false
+      });
     } catch (e) {
       if (e.response.status !== 401)
         notification.error({
@@ -42,11 +46,15 @@ class App extends Component {
     }
   };
 
-  handleLogin = () => this.setState({ isAuth: true });
+  handleLogin = () => {
+    console.log(5);
+    this.setState({ isAuth: true });
+  };
 
   handleUnauth = () => {
+    console.log("1");
     this.setState({ isAuth: false, user: {} });
-  }
+  };
 
   handleLogout = async () => {
     this.setState({ isAuth: false, isLoading: true });
@@ -59,102 +67,116 @@ class App extends Component {
   };
 
   render() {
-    const { isAuth, isLoading } = this.state;
+    const { isAuth, isLoading, user } = this.state;
     return isLoading ? (
       <Loading />
     ) : (
-        <Router>
-          <Header />
-          <main className="container">
-            <Switch>
-              <Route
-                path="/logout"
-                component={() => {
-                  this.handleLogout();
-                  return <Redirect to="/login" />;
-                }}
-              />
-              <ProtectedRoute
-                exact
-                path="/profile"
-                isAuth={isAuth}
-                component={(props) =>
-                  <Profile
-                    {...props}
-                    handleUnauth={this.handleUnauth}
-                  />}
-              />
-              <ProtectedRoute
-                path="/posts/live"
-                isAuth={isAuth}
-                component={(props) =>
-                  <Post
-                    handleUnauth={this.handleUnauth}
-                    {...props} postType="live"
-                  />}
-              />
-              <ProtectedRoute
-                isAuth={isAuth}
-                path="/posts/draft"
-                component={(props) =>
-                  <Post
-                    handleUnauth={this.handleUnauth}
-                    {...props}
-                    postType="draft"
-                  />}
-
-              />
-              <ProtectedRoute
-                path="/posts/new"
-                isAuth={isAuth}
-                component={PostForm}
-              />
-              <ProtectedRoute
-                exact
-                path="/post/public-service/:id/edit"
-                isAuth={isAuth}
-                render={props => (
-                  <PostForm postFormType="public service" {...props} />
-                )}
-              />
-              <ProtectedRoute
-                exact
-                path="/post/event/:id/edit"
-                isAuth={isAuth}
-                render={props => <PostForm postFormType="event" {...props} />}
-              />
-              <ProtectedRoute
-                isAuth={isAuth}
-                path="/post/event/:category/:id"
-                render={props => <Event {...props} />}
-              />
-              <ProtectedRoute
-                isAuth={isAuth}
-                path="/post/public-service/:category/:id"
-                render={props => <PublicService {...props} />}
-              />
-              <ProtectedRoute isAuth={isAuth} path="/posts" component={Posts} />
-              {!isAuth ? (
-                <>
-                  <Route
-                    path="/login"
-                    component={props => (
-                      <Login {...props} handleLogin={this.handleLogin} />
-                    )}
-                  />
-                  <Route path="/signup" component={CreateProfile} />
-                  <Route exact path="/" component={Home} />
-                </>
-              ) : (
-                  <Redirect to={`/profile`} />
-                )}
-
-              <Route component={PageNotFound} />
-            </Switch>
+      <Router>
+        <>
+          {user.role !== "admin" && <Header showHamburger={isAuth} />}
+          <main
+            className="container"
+            style={isAuth ? {} : { minHeight: "calc(100vh - (129px + 70px))" }}
+          >
+            {!isAuth ? (
+              <Switch>
+                <Route
+                  path="/login"
+                  component={props => (
+                    <Login {...props} handleLogin={this.handleLogin} />
+                  )}
+                />
+                <Route path="/signup" component={CreateProfile} />
+                <Route component={PageNotFound} />
+              </Switch>
+            ) : user.role === "member" ? (
+              <Switch>
+                <Route
+                  path="/logout"
+                  component={() => {
+                    this.handleLogout();
+                    // return <h1>Hi</h1>;
+                    return <Redirect to="/login" />;
+                  }}
+                />
+                <ProtectedRoute
+                  exact
+                  path="/profile"
+                  isAuth={isAuth}
+                  component={props => (
+                    <Profile {...props} handleUnauth={this.handleUnauth} />
+                  )}
+                />
+                <ProtectedRoute
+                  path="/posts/live"
+                  isAuth={isAuth}
+                  component={props => (
+                    <Post
+                      handleUnauth={this.handleUnauth}
+                      {...props}
+                      postType="live"
+                    />
+                  )}
+                />
+                <ProtectedRoute
+                  isAuth={isAuth}
+                  path="/posts/draft"
+                  component={props => (
+                    <Post
+                      handleUnauth={this.handleUnauth}
+                      {...props}
+                      postType="draft"
+                    />
+                  )}
+                />
+                <ProtectedRoute
+                  path="/posts/new"
+                  isAuth={isAuth}
+                  component={PostForm}
+                />
+                <ProtectedRoute
+                  exact
+                  path="/post/public-service/:id/edit"
+                  isAuth={isAuth}
+                  render={props => (
+                    <PostForm postFormType="public service" {...props} />
+                  )}
+                />
+                <ProtectedRoute
+                  exact
+                  path="/post/event/:id/edit"
+                  isAuth={isAuth}
+                  render={props => <PostForm postFormType="event" {...props} />}
+                />
+                <ProtectedRoute
+                  isAuth={isAuth}
+                  path="/post/event/:category/:id"
+                  render={props => <Event {...props} />}
+                />
+                <ProtectedRoute
+                  isAuth={isAuth}
+                  path="/post/public-service/:category/:id"
+                  render={props => <PublicService {...props} />}
+                />
+                <ProtectedRoute
+                  isAuth={isAuth}
+                  path="/posts"
+                  component={Posts}
+                />
+                <Route component={PageNotFound} />
+              </Switch>
+            ) : (
+              user.role === "admin" && (
+                <Switch>
+                  <h2>Welcome admin</h2>
+                </Switch>
+              )
+            )}
           </main>
-          <Footer />
-        </Router>
-      );
+          {user.role !== "admin" && <Footer />}
+        </>
+      </Router>
+    );
   }
 }
 
--- client/src/components/pages/member/Home/index.js
+++ client/src/components/pages/member/Home/index.js
@@ -4,7 +4,8 @@ import { Link } from "react-router-dom";
 import Button from "components/utils/Button";
 import "./style.css";
 
-function Home() {
+function Home(props) {
+  console.log(props)
   return (
     <section className="home">
       <p className="home--desc">
