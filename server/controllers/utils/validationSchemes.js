5dff8fafb699167abfeabdb4e7667dfb3d6f0980
knanahassouna1 <knanahassouna2@gmail.com>
2019-06-26 16:25:53 +0300

    edit the scheme of login route

--- server/controllers/utils/validationSchemes.js
+++ server/controllers/utils/validationSchemes.js
@@ -1,6 +1,6 @@
-const yup = require('yup');
+const yup = require("yup");
 
-const loginSchema = require('./loginSchema');
+const loginSchema = require("./loginSchema");
 
 exports.loginSchema = loginSchema;
 
@@ -29,7 +29,7 @@ exports.userPostSchema = yup.object().shape({
   website: yup
     .string()
     .matches(
-      /^((https?|ftp|smtp):\/\/)?(www.)?[a-z0-9]+(\.[a-z]{2,}){1,3}(#?\/?[a-zA-Z0-9#]+)*\/?(\?[a-zA-Z0-9-_]+=[a-zA-Z0-9-%]+&?)?$/,
+      /^((https?|ftp|smtp):\/\/)?(www.)?[a-z0-9]+(\.[a-z]{2,}){1,3}(#?\/?[a-zA-Z0-9#]+)*\/?(\?[a-zA-Z0-9-_]+=[a-zA-Z0-9-%]+&?)?$/
     )
     .required(),
   address: yup
@@ -46,28 +46,26 @@ exports.userPostSchema = yup.object().shape({
     .required(),
   zipCode: yup
     .string()
-    .matches(
-      /^([Gg][Ii][Rr] 0[Aa]{2})|((([A-Za-z][0-9]{1,2})|(([A-Za-z][A-Ha-hJ-Yj-y][0-9]{1,2})|(([A-Za-z][0-9][A-Za-z])|([A-Za-z][A-Ha-hJ-Yj-y][0-9]?[A-Za-z])))) [0-9][A-Za-z]{2})$/
-    )
+    .matches(/^[0-9]{5}(?:-[0-9]{4})?$/)
     .required(),
   facebook: yup
     .string()
     .matches(
-      /^((https?|ftp|smtp):\/\/)?(www.)?[a-z0-9]+(\.[a-z]{2,}){1,3}(#?\/?[a-zA-Z0-9#]+)*\/?(\?[a-zA-Z0-9-_]+=[a-zA-Z0-9-%]+&?)?$/,
+      /^((https?|ftp|smtp):\/\/)?(www.)?[a-z0-9]+(\.[a-z]{2,}){1,3}(#?\/?[a-zA-Z0-9#]+)*\/?(\?[a-zA-Z0-9-_]+=[a-zA-Z0-9-%]+&?)?$/
     ),
   twitter: yup
     .string()
     .matches(
-      /^((https?|ftp|smtp):\/\/)?(www.)?[a-z0-9]+(\.[a-z]{2,}){1,3}(#?\/?[a-zA-Z0-9#]+)*\/?(\?[a-zA-Z0-9-_]+=[a-zA-Z0-9-%]+&?)?$/,
+      /^((https?|ftp|smtp):\/\/)?(www.)?[a-z0-9]+(\.[a-z]{2,}){1,3}(#?\/?[a-zA-Z0-9#]+)*\/?(\?[a-zA-Z0-9-_]+=[a-zA-Z0-9-%]+&?)?$/
     ),
   instagram: yup
     .string()
     .matches(
-      /^((https?|ftp|smtp):\/\/)?(www.)?[a-z0-9]+(\.[a-z]{2,}){1,3}(#?\/?[a-zA-Z0-9#]+)*\/?(\?[a-zA-Z0-9-_]+=[a-zA-Z0-9-%]+&?)?$/,
-    ),
+      /^((https?|ftp|smtp):\/\/)?(www.)?[a-z0-9]+(\.[a-z]{2,}){1,3}(#?\/?[a-zA-Z0-9#]+)*\/?(\?[a-zA-Z0-9-_]+=[a-zA-Z0-9-%]+&?)?$/
+    )
 });
 
 exports.fetchPostSchema = yup.object().shape({
   postId: yup.number(),
-  postType: yup.string().matches(/(\bevent\b)|(\bpublic_service\b)/),
+  postType: yup.string().matches(/(\bevent\b)|(\bpublic_service\b)/)
 });
