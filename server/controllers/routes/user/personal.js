3642c956a91d6e676d4042a44135ed8507b28c4e
knanahassouna1 <knanahassouna2@gmail.com>
2019-06-26 16:43:48 +0300

    update cookie when update personal

--- server/controllers/routes/user/personal.js
+++ server/controllers/routes/user/personal.js
@@ -1,45 +1,62 @@
-const { compare } = require('bcryptjs');
-
-const personalSchema = require('../../utils/personalSchema');
-const { getPassword } = require('./../../../database/queries/getPassword');
-const { updatePersonalDataQuery } = require('./../../../database/queries/updatePersonalData');
+const { compare } = require("bcryptjs");
+const { genCookie } = require("../../utils/helper");
+const personalSchema = require("../../utils/personalSchema");
+const { getPassword } = require("./../../../database/queries/getPassword");
+const {
+  updatePersonalDataQuery
+} = require("./../../../database/queries/updatePersonalData");
 
 exports.updatePersonal = (req, res, next) => {
-  const {
-    oldPassword, firstName, lastName, email,
-  } = req.body;
+  const { oldPassword, firstName, lastName, email } = req.body;
   const { id } = req.user;
 
   personalSchema
     .isValid({
       firstName,
       lastName,
-      email,
+      email
     })
-    .then((valid) => {
+    .then(valid => {
       if (!valid) {
-        const objError = new Error('Bad Request');
+        const objError = new Error("Bad Request");
         objError.statusCode = 400;
         throw objError;
       }
       return getPassword(id);
     })
     .then(dbRes => compare(oldPassword, dbRes.rows[0].password))
-    .then((passMatch) => {
-      if (passMatch) return updatePersonalDataQuery(firstName, lastName, email, id);
-      const objError = new Error('Retry, password is wrong');
+    .then(passMatch => {
+      if (passMatch)
+        return updatePersonalDataQuery(firstName, lastName, email, id);
+      const objError = new Error("Retry, password is wrong");
       objError.statusCode = 400;
       throw objError;
     })
-    .then(() => res.send({
-      data: 'Personal Data Updated Successfully',
-      statusCode: 200,
-    }))
-    .catch((e) => {
+    .then(({ rows }) => {
+      const user = rows[0];
+      const { password: pass, ...userResult } = user;
+      const cookieOptions = {
+        expires: new Date(Date.now() + 1000 * 60 * 60 * 24 * 30)
+      };
+      res.cookie(
+        "jwt",
+        genCookie({
+          ...userResult,
+          role: user.id === 1 ? "admin" : "member"
+        }),
+        cookieOptions
+      );
+      res.send({
+        data: "Personal Data Updated Successfully",
+        statusCode: 200
+      });
+    })
+    .catch(e => {
       const { statusCode, message } = e;
       if (statusCode) {
         res.status(statusCode).send({
-          statusCode, error: message,
+          statusCode,
+          error: message
         });
       } else {
         next(e);
